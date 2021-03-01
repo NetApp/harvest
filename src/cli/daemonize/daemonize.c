c645d319d42c90012709625d7ebcdd4d7e835fd0
Vachagan Gratian <vgratian@utopianlab.am>
2021-03-01 14:36:30 +0400

    fix bug truncating last arg, cleanup

--- src/cli/daemonize/daemonize.c
+++ src/cli/daemonize/daemonize.c
@@ -5,27 +5,52 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <syslog.h>
+#include <errno.h>
+
+/*
+    Executes command as a daemon process. Unlike what 
+    is done usually (two forks and exit), we perform
+    execv after second fork, such that the daemon has
+    it's own cmdline identity. This is helpful to
+    trace status of pollers.
+
+    Script is only intended to daemonize Harvest pollers.
+    Daemonizing other processes should be ok, as long as
+    the command line arguments are correct, but no
+    guarantee.
+
+    The daemonize function is implemented in the quick
+    and dirty way: some system calls would need better
+    error-checking for a more safe implementation.
+
+    Usage:
+        ./daemonize <executable> [args...]
+
+    Arguments:
+        - executable    path to executable program
+        - args          optional, passed to daemon unchanged
+*/
+
 
 int daemonize(char *bin, char *args[]) {
 
     // open syslog to send messages
     openlog("harvest daemonize", LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "entrypoint");
 
     if (fork() != 0)
         return 0; // parent exits
     // child continues...
-    syslog(LOG_INFO, "after first fork");
 
     // get new session ID
-    if (setsid() == -1)
+    if (setsid() == -1) {
+        syslog(LOG_ERR, "setsid: %s", strerror(errno));
         return -1;
+    }
 
     // second fork, so we are not session leader
     if (fork() != 0)
         return 0;
     // (grand) child continues
-    syslog(LOG_INFO, "after second fork");
 
     // clean file permissions
     umask(0);
@@ -45,44 +70,37 @@ int daemonize(char *bin, char *args[]) {
     dup2(STDIN_FILENO, STDOUT_FILENO);
     dup2(STDIN_FILENO, STDERR_FILENO);
 
-    // finally execv so the poller has its own cmdline identity
-    syslog(LOG_INFO, "before execv");
+    // finally execv so the daemon has its own cmdline identity
     execv(bin, args);
-    syslog(LOG_INFO, "after execv, oops!");
-    return -1; // if we got here, something went wrong
+     // if we got here, something went wrong
+    syslog(LOG_ERR, "execv: %s", strerror(errno));
+    return -1;
 }
 
+int main(int argc, char* argv[]) {
 
-int main(int argc, char *argv[]) {
-
-    if (argc == 1 || argv[1] == "-h" || argv[1] == "--help") {
-        printf("Usage: ./daemonize <program path> <arg vector>\n");
+    // at least one argument is required
+    if (argc < 2 || argv[1] == "-h" || argv[1] == "--help") {
+        printf("Usage: ./daemonize <executable> [args...]\n");
         exit(0);
     }
 
-    // construct arg vector, so we can detect errors more easily
-    char *Args[argc-1];
-    char ArgsFlat[200];
+    // construct path to executable and arg vector
+    // this is done here, so errors are detected early on
+    char* Args[100];
+    char path[100];
     int i;
 
-    //ArgsFlat = "";
+    strcpy(path, argv[1]);
 
-    // null terminated vector
-    Args[0] = strrchr(argv[1], '/');
-    if (Args[0] == NULL)
-        Args[0] = argv[1];
-    else
-        Args[0]++;
+    Args[0] = "poller";
 
-    for (i=2; i<argc; i++)
-        Args[i-1] = argv[i];
-    Args[i-1] = NULL;
+    for (i=1; i<argc-1; i++) {
+        Args[i] = argv[i+1];
+    }
 
-    for (i=0; i<argc-1; i++)
-        strcat(ArgsFlat, Args[i]);
+    // arg vector should be null-terminated
+    Args[i] = NULL;
 
-    printf("Daemonizing (%s) [%s]\n", argv[1], ArgsFlat);
-
-
-    exit( daemonize(argv[1], Args) );
+    return daemonize(path, Args);
 }
