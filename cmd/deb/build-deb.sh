c98a527129592105491b6eba1e4de92e5f95962c
Vachagan Gratian <vgratian@utopianlab.am>
2021-04-07 12:55:18 +0400

    build script for DEB

--- cmd/deb/build-deb.sh
+++ cmd/deb/build-deb.sh
@@ -17,15 +17,29 @@ if [ -z "$SRC" ]; then
 fi
 
 # create build directory tree
-rm -rf "$BUILD"
+info "creating package directories"
+rm -rf "$BUILD" && mkdir -p "$BUILD" && cd "$BUILD"
 mkdir -p "$BUILD/opt/harvest/"
-cd "$BUILD"
-cp -r "$SRC/*" "opt/harvest/"
+mkdir -p "$BUILD/etc/harvest/"
+mkdir -p "$BUILD/var/log/harvest/"
+mkdir -p "$BUILD/var/run/harvest/"
+cp -r "$SRC/src/" "opt/harvest/"
+cp -r "$SRC/cmd/" "opt/harvest/"
+cp -r "$SRC/docs/" "opt/harvest/"
+cp -r "$SRC/README.md" "opt/harvest/"
+cp -r "$SRC/conf/" "etc/harvest/"
+cp -r "$SRC/grafana/" "etc/harvest/"
+cp -r "$SRC/harvest.yml" "etc/harvest/"
+
+info "creating DEB control file"
 mkdir "DEBIAN"
 echo "Package: harvest" > "DEBIAN/control"
 echo "Version: $HARVEST_VERSION-$HARVEST_RELEASE" >> "DEBIAN/control"
+#echo "Section: base" > "DEBIAN/control"
+echo "Priority: optional" > "DEBIAN/control"
 echo "Architecture: $HARVEST_ARCH" >> "DEBIAN/control"
 cat "$SRC/cmd/deb/harvest.control" >> "DEBIAN/control"
+cp "$SRC/cmd/install.sh" "DEBIAN/preinst"
 
 # build binaries
 info "compiling binaries..."
@@ -36,21 +50,14 @@ if [ ! $? -eq 0 ]; then
     exit 1
 fi
 
-# install directores
-info "creating package directories"
-cd "$BUILD"
-mkdir -p "etc/harvest/"
-mkdir -p "var/log/harvest/"
-mkdir -p "var/run/harvest/"
-mv -r "opt/harvest/config" "etc/harvest/"
-
 # build deb package
 mkdir -p "$SRC/dist/$HARVEST_VERSION-$HARVEST_RELEASE"
-dpkg-deb --build "$BUILD" "$SRC/dist/$HARVEST_VERSION-$HARVEST_RELEASE"
+dpkg-deb --build "$BUILD" "$SRC/dist/$HARVEST_VERSION-$HARVEST_RELEASE_$HARVEST_ARCH.deb"
 if [ ! $? -eq 0 ]; then
     error "dpkg build failed"
     exit 1
 fi
 
+rm -Rf "$BUILDIR"
 info "DEB package ready for distribution. Have a nice evening!"
 exit 0
