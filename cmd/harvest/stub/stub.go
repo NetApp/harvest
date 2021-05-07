fc42da4161e877e65832b579dbc9a85c1e41ee63
Vachagan Gratian <vgratian@utopianlab.am>
2021-05-07 14:10:12 +0400

    cleanup/expand stub, add exporter

--- cmd/harvest/stub/stub.go
+++ cmd/harvest/stub/stub.go
@@ -14,14 +14,38 @@ import (
 	"strings"
 )
 
+var usage = `
+Harvest 2.0 Stub utility
+
+Tool for developers to create new collector, plugin or exporter
+
+Usage: harvest new [collector | plugin | exporter ]
+`
+
+var (
+	harvestHomePath string
+	harvestConfPath string
+)
+
 func Run() {
 
-	var harvest_path string
-	if harvest_path = os.Getenv("HARVEST_HOME"); harvest_path == "" {
-		harvest_path = "/opt/harvest/"
+	var (
+		err error
+	)
+	if harvestHomePath = os.Getenv("HARVEST_HOME"); harvestHomePath == "" {
+		harvestHomePath = "/opt/harvest/"
 	}
 
-	var err error
+	if harvestConfPath = os.Getenv("HARVEST_CONF"); harvestConfPath == "" {
+		harvestConfPath = "/etc/harvest/"
+	}
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-h" || os.Args[1] == "--help" || os.Args[1] == "help" {
+			fmt.Println(usage)
+			os.Exit(0)
+		}
+	}
 
 	if len(os.Args) < 3 {
 		fmt.Println("What to create? (choose: collector, plugin, exporter)")
@@ -29,9 +53,11 @@ func Run() {
 	}
 
 	if os.Args[2] == "collector" {
-		err = new_collector(harvest_path)
+		err = newCollector()
 	} else if os.Args[2] == "plugin" {
-		err = new_plugin(harvest_path)
+		err = newPlugin()
+	} else if os.Args[2] == "exporter" {
+		err = newExporter()
 	} else {
 		fmt.Printf("Sorry, can't create %s\n", os.Args[2])
 		os.Exit(1)
@@ -44,70 +70,105 @@ func Run() {
 	os.Exit(0)
 }
 
-func new_plugin(harvest_path string) error {
+func newPlugin() error {
 	var (
 		name, collector string
 		err             error
 		data            []byte
 	)
 
-	name = get_name()
+	name = getName()
 
+	fmt.Println("For which collector is this plugin?")
 	fmt.Printf("collector: ")
 	if _, err = fmt.Scanln(&collector); err != nil {
 		return err
 	}
 
-	if data, err = ioutil.ReadFile(path.Join(harvest_path, "template/", "plugin.go")); err != nil {
+	if data, err = ioutil.ReadFile(path.Join(harvestHomePath, "cmd/harvest/stub/_plugin.go_")); err != nil {
 		return err
 	}
 
-	fp := path.Join(harvest_path, "src/", "collectors/", strings.ToLower(collector), "plugins/", strings.ToLower(name))
+	fp := path.Join(harvestHomePath, "cmd/collectors/", strings.ToLower(collector), "plugins/", strings.ToLower(name))
 
 	if err = os.MkdirAll(fp, 0755); err != nil {
 		return err
 	}
 
-	fp = path.Join(fp, strings.ToLower(name)+".go")
-
 	data = bytes.ReplaceAll(data, []byte("PLUGIN"), []byte(name))
 
+	fp = path.Join(fp, strings.ToLower(name)+".go")
 	if err = ioutil.WriteFile(fp, data, 0644); err != nil {
 		return err
 	}
 
 	fmt.Printf("Created source file [%s]\n", fp)
-	fmt.Printf("Run \"harvest build collector %s\" when you are ready\n", strings.ToLower(name))
+	fmt.Println("Run \"make collectors\" when you are ready")
 	fmt.Println("Happy coding!")
 
 	return nil
 }
 
-func new_collector(harvest_path string) error {
+func newExporter() error {
+	var (
+		name string
+		data []byte
+		err  error
+	)
+
+	name = getName()
+
+	if data, err = ioutil.ReadFile(path.Join(harvestHomePath, "cmd/harvest/stub/_exporter.go_")); err != nil {
+		return err
+	}
+
+	fp := path.Join(harvestHomePath, "cmd/exporters/", strings.ToLower(name))
+
+	if err = os.MkdirAll(fp, 0755); err != nil {
+		return err
+	}
+
+	data = bytes.ReplaceAll(data, []byte("EXPORTER"), []byte(name))
+
+	fp = path.Join(fp, strings.ToLower(name)+".go")
+	if err = ioutil.WriteFile(fp, data, 0644); err != nil {
+		return err
+	}
+
+	fmt.Printf("Created source file [%s]\n", fp)
+	fmt.Println("Run \"make exporters\" when you are ready")
+	fmt.Println("Happy coding!")
+
+	return nil
+}
+
+func newCollector() error {
 	var (
 		name, object string
 		data         []byte
 		err          error
 	)
 
-	name = get_name()
+	name = getName()
 
+	fmt.Println("What object does this collector collect?")
+	fmt.Println("(choose a name that best describes your metrics/instances)")
 	fmt.Printf("object: ")
 	if _, err = fmt.Scanln(&object); err != nil {
 		return err
 	}
 
-	if tfp, err := create_template(harvest_path, name, object); err != nil {
+	if tfp, err := createTemplate(name, object); err != nil {
 		return err
 	} else {
 		fmt.Printf("Created collector config [%s]\n", tfp)
 	}
 
-	if data, err = ioutil.ReadFile(path.Join(harvest_path, "template/", "collector.go")); err != nil {
+	if data, err = ioutil.ReadFile(path.Join(harvestHomePath, "cmd/harvest/stub/_collector.go_")); err != nil {
 		return err
 	}
 
-	fp := path.Join(harvest_path, "src/", "collectors/", strings.ToLower(name))
+	fp := path.Join(harvestHomePath, "cmd/", "collectors/", strings.ToLower(name))
 
 	if err = os.MkdirAll(fp, 0755); err != nil {
 		return err
@@ -122,13 +183,13 @@ func new_collector(harvest_path string) error {
 	}
 
 	fmt.Printf("Created source file [%s]\n", fp)
-	fmt.Printf("Run \"harvest build collector %s\" when you are ready\n", strings.ToLower(name))
+	fmt.Println("Run \"make collectors\" when you are ready")
 	fmt.Println("Happy coding!")
 
 	return nil
 }
 
-func get_name() string {
+func getName() string {
 
 	var name string
 
@@ -141,7 +202,7 @@ func get_name() string {
 	return name
 }
 
-func create_template(harvest_path, collector, object string) (string, error) {
+func createTemplate(collector, object string) (string, error) {
 
 	t := node.NewS("")
 	t.NewChildS("collector", collector)
@@ -163,7 +224,7 @@ func create_template(harvest_path, collector, object string) (string, error) {
 	export := t.NewChildS("export_options", "")
 	export.NewChildS("include_all_labels", "True")
 
-	fp := path.Join(harvest_path, "conf/", strings.ToLower(collector))
+	fp := path.Join(harvestConfPath, "conf/", strings.ToLower(collector))
 
 	if err := os.MkdirAll(fp, 0755); err != nil {
 		return "", err
