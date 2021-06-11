195f2f64c9e30ca6d96217c999ba31a72e60fedc
Hardik Leuva <hardikl@netapp.com>
2021-06-11 18:08:35 +0530

    PrometheusPort should be int

--- cmd/poller/poller.go
+++ cmd/poller/poller.go
@@ -753,7 +753,7 @@ func init() {
 	flags.BoolVar(&args.Daemon, "daemon", false, "Start as daemon")
 	flags.IntVarP(&args.LogLevel, "loglevel", "l", 2, "Logging level (0=trace, 1=debug, 2=info, 3=warning, 4=error, 5=critical)")
 	flags.IntVar(&args.Profiling, "profiling", 0, "If profiling port > 0, enables profiling via localhost:PORT/debug/pprof/")
-	flags.StringVar(&args.PromPort, "promPort", "", "Prometheus Port")
+	flags.IntVar(&args.PromPort, "promPort", 0, "Prometheus Port")
 	flags.StringVar(&args.Config, "config", configPath, "harvest config file path")
 	flags.StringSliceVarP(&args.Collectors, "collectors", "c", []string{}, "only start these collectors (overrides harvest.yml)")
 	flags.StringSliceVarP(&args.Objects, "objects", "o", []string{}, "only start these objects (overrides collector config)")
