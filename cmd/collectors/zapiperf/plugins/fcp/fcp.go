3c286ebe8f60c85913e5b37f39c28789e1c94331
Hardik Leuva <hardikl@netapp.com>
2021-08-27 18:16:37 +0530

    fix: fcp plugin to support 7mode counter

--- cmd/collectors/zapiperf/plugins/fcp/fcp.go
+++ cmd/collectors/zapiperf/plugins/fcp/fcp.go
@@ -26,12 +26,14 @@ func (me *Fcp) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 	var err error
 
 	if read = data.GetMetric("read_data"); read == nil {
+		// Check for 7 mode fcp counters, as they starts with fcp_.
 		if read = data.GetMetric("fcp_read_data"); read == nil {
 			return nil, errors.New(errors.ERR_NO_METRIC, "read_data")
 		}
 	}
 
 	if write = data.GetMetric("write_data"); write == nil {
+		// Check for 7 mode fcp counters, as they starts with fcp_.
 		if write = data.GetMetric("fcp_write_data"); write == nil {
 			return nil, errors.New(errors.ERR_NO_METRIC, "write_data")
 		}
