d022225ea18ef7e17ff17902398e641ef77bd239
Chris Grindstaff <chris@gstaff.org>
2021-10-07 11:30:52 -0400

    feat: Harvest 7-mode dashboards should be provisioned
    docs: Update Grafana folder names for cDOT and 7-mode

--- integration/test/dashboard_test.go
+++ integration/test/dashboard_test.go
@@ -1,4 +1,5 @@
-//+build regression dashboard
+//go:build regression || dashboard
+// +build regression dashboard
 
 package main
 
@@ -67,7 +68,7 @@ func (suite *DashboardImportTestSuite) TestImport() {
 }
 
 func (suite *DashboardImportTestSuite) TestCModeDashboardCount() {
-	folderId := GetFolderId("Harvest 2.0", suite.T())
+	folderId := GetFolderId("Harvest 2.0 - cDOT", suite.T())
 	expectedName := []string{"Harvest Metadata", "NetApp Detail: Aggregate", "NetApp Detail: Cluster",
 		"NetApp Detail: Disk", "NetApp Detail: LUN", "NetApp Detail: Network", "NetApp Detail: Network  - Details",
 		"NetApp Detail: Network with NVMe/FC", "NetApp Detail: Node", "NetApp Detail: Node - Details",
@@ -79,7 +80,7 @@ func (suite *DashboardImportTestSuite) TestCModeDashboardCount() {
 
 func (suite *DashboardImportTestSuite) TestSevenModeDashboardCount() {
 
-	folderId := GetFolderId("7 mode", suite.T())
+	folderId := GetFolderId("Harvest 2.0 - 7-mode", suite.T())
 	expectedName := []string{"NetApp Detail: Aggregate 7 mode", "NetApp Detail: Cluster 7 mode",
 		"NetApp Detail: Disk 7 mode", "NetApp Detail: LUN 7 mode", "NetApp Detail: Network 7 mode", "NetApp Detail: Network with NVMe/FC 7 mode",
 		"NetApp Detail: Node 7 mode", "NetApp Detail: Shelf 7 mode", "NetApp Detail: Volume 7 mode"}
