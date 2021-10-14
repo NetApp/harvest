591450ddd808ea42da210c51945ccec8d845996a
Hardik Leuva <hardikl@netapp.com>
2021-10-14 16:09:38 +0530

    feat: quota plugin for qtree object - handled batching and - mapped to 0

--- cmd/collectors/zapi/plugins/quota/quota.go
+++ cmd/collectors/zapi/plugins/quota/quota.go
@@ -11,16 +11,19 @@ import (
 	"goharvest2/pkg/errors"
 	"goharvest2/pkg/matrix"
 	"goharvest2/pkg/tree/node"
-	"math"
 	"strconv"
 	"strings"
 )
 
+const QuotaBatchSize = "500"
+
+// Quota plugin is needed to match qtrees with quotas.
 type Quota struct {
 	*plugin.AbstractPlugin
-	data           map[string]*matrix.Matrix
+	data           *matrix.Matrix
 	instanceKeys   map[string]string
 	instanceLabels map[string]*dict.Dict
+	batchSize      string
 	client         *zapi.Client
 	query          string
 }
@@ -49,9 +52,22 @@ func (my *Quota) Init() error {
 	my.query = "quota-report-iter"
 	my.Logger.Debug().Msg("plugin connected!")
 
-	my.data = make(map[string]*matrix.Matrix)
+	my.data = matrix.New(my.Parent+".Qtree", "qtree", "qtree")
 	my.instanceKeys = make(map[string]string)
 	my.instanceLabels = make(map[string]*dict.Dict)
+
+	exportOptions := node.NewS("export_options")
+	instanceKeys := exportOptions.NewChildS("instance_keys", "")
+
+	// apply all instance keys, instance labels from parent (qtree.yaml) to all quota metrics
+	//parent instancekeys would be added in plugin metrics
+	for _, parentKeys := range my.ParentParams.GetChildS("export_options").GetChildS("instance_keys").GetAllChildContentS() {
+		instanceKeys.NewChildS("", parentKeys)
+	}
+	// parent instacelabels would be added in plugin metrics
+	for _, parentLabels := range my.ParentParams.GetChildS("export_options").GetChildS("instance_labels").GetAllChildContentS() {
+		instanceKeys.NewChildS("", parentLabels)
+	}
 
 	objects := my.Params.GetChildS("objects")
 	if objects == nil {
@@ -59,162 +75,156 @@ func (my *Quota) Init() error {
 	}
 
 	for _, obj := range objects.GetAllChildContentS() {
-
 		metricName, display := collector.ParseMetricName(obj)
 
-		my.instanceLabels[metricName] = dict.New()
-
-		my.data[metricName] = matrix.New(my.Parent+".Qtree", "qtree", "qtree_"+display)
-		my.data[metricName].SetGlobalLabel("datacenter", my.ParentParams.GetChildContentS("datacenter"))
-		my.data[metricName].SetGlobalLabel("cluster", my.client.Name())
-
-		exportOptions := node.NewS("export_options")
-		instanceLabels := exportOptions.NewChildS("instance_labels", "")
-		instanceKeys := exportOptions.NewChildS("instance_keys", "")
-
-		// apply all instance keys, instance labels from parent (qtree.yaml) to all quota metrics
-		//parent instancekeys would be added in plugin metrics
-		for _, parentKeys := range my.ParentParams.GetChildS("export_options").GetChildS("instance_keys").GetAllChildContentS() {
-			instanceKeys.NewChildS("", parentKeys)
-		}
-		// parent instacelabels would be added in plugin metrics
-		for _, parentLabels := range my.ParentParams.GetChildS("export_options").GetChildS("instance_labels").GetAllChildContentS() {
-			instanceKeys.NewChildS("", parentLabels) //strings.ReplaceAll(parentLabels, "_", "-"))
+		metric, err := my.data.NewMetricFloat64(metricName)
+		if err != nil {
+			my.Logger.Error().Stack().Err(err).Msg("add metric")
+			return err
 		}
 
-		if strings.HasPrefix(obj, "^") {
-			if strings.HasPrefix(obj, "^^") {
-				my.instanceKeys[metricName] = metricName
-				my.instanceLabels[metricName].Set(metricName, display)
-				instanceKeys.NewChildS("", display)
-				my.Logger.Debug().Msgf("added instance key: (%s) (%s) [%s]", metricName, objects.GetNameS(), display)
-			} else {
-				my.instanceLabels[metricName].Set(metricName, display)
-				instanceLabels.NewChildS("", display)
-				my.Logger.Debug().Msgf("added instance label: (%s) (%s) [%s]", metricName, objects.GetNameS(), display)
-			}
-		} else {
-			metric, err := my.data[metricName].NewMetricFloat64(metricName)
-			if err != nil {
-				my.Logger.Error().Stack().Err(err).Msg("add metric")
-				return err
-			}
-			metric.SetName(display)
-			my.Logger.Debug().Msgf("added metric: (%s) (%s) [%s] %s", metricName, objects.GetNameS(), display, metric)
-		}
-
-		my.Logger.Debug().Msgf("added data for [%s] with %d metrics", metricName, len(my.data[metricName].GetMetrics()))
-		my.data[metricName].SetExportOptions(exportOptions)
+		metric.SetName(display)
+		my.Logger.Debug().Msgf("added metric: (%s) [%s] %s", metricName, display, metric)
 	}
 
-	my.Logger.Debug().Msgf("initialized with data [%d] objects", len(my.data))
+	my.Logger.Debug().Msgf("added data with %d metrics", len(my.data.GetMetrics()))
+	my.data.SetExportOptions(exportOptions)
+
+	// batching the request
+	if my.client.IsClustered() {
+		if b := my.Params.GetChildContentS("batch_size"); b != "" {
+			if _, err := strconv.Atoi(b); err == nil {
+				my.batchSize = b
+				my.Logger.Info().Msgf("using batch-size [%s]", my.batchSize)
+			}
+		} else {
+			my.batchSize = QuotaBatchSize
+			my.Logger.Trace().Msgf("using default batch-size [%s]", QuotaBatchSize)
+		}
+	}
+
 	return nil
 }
 
 func (my *Quota) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 
 	var (
-		result *node.Node
-		quotas []*node.Node
-		err    error
+		request, result *node.Node
+		quotas          []*node.Node
+		tag             string
+		err             error
 	)
-
-	if result, err = my.client.InvokeRequestString(my.query); err != nil {
-		return nil, err
-	}
-
-	if x := result.GetChildS("attributes-list"); x != nil {
-		quotas = x.GetChildren()
-	}
-
-	if len(quotas) == 0 {
-		return nil, errors.New(errors.ERR_NO_INSTANCE, "no quota instances found")
-	}
-
-	my.Logger.Debug().Msgf("fetching %d quota counters", len(quotas))
 
 	var output []*matrix.Matrix
 
 	// Purge and reset data
-	for _, data1 := range my.data {
-		data1.PurgeInstances()
-		data1.Reset()
+	my.data.PurgeInstances()
+	my.data.Reset()
+
+	// Set all global labels from zapi.go if already not exist
+	my.data.SetGlobalLabels(data.GetGlobalLabels())
+
+	request = node.NewXmlS(my.query)
+	if my.client.IsClustered() && my.batchSize != "" {
+		request.NewChildS("max-records", my.batchSize)
 	}
 
-	for key, quota := range quotas {
+	tag = "initial"
 
-		tree := quota.GetChildContentS("tree")
-		volume := quota.GetChildContentS("volume")
-		vserver := quota.GetChildContentS("vserver")
+	for {
 
-		for attribute, data1 := range my.data {
+		result, tag, err = my.client.InvokeBatchRequest(request, tag)
 
-			objectElem := quota.GetChildS(attribute)
-			if objectElem == nil {
-				my.Logger.Warn().Msgf("no [%s] instances on this system", attribute)
-				continue
-			}
-
-			if ok := quota.GetChildContentS(attribute); ok != "" {
-				instanceKey := vserver + "." + volume + "." + tree + "." + strconv.Itoa(key)
-				my.Logger.Info().Msgf("instance: %s", instanceKey)
-				instance, err := data1.NewInstance(instanceKey)
-
-				if err != nil {
-					my.Logger.Debug().Msgf("add (%s) instance: %v", attribute, err)
-					return nil, err
-				}
-
-				my.Logger.Debug().Msgf("add (%s) instance: %s.%s.%s", attribute, vserver, volume, tree)
-
-				qtreeInstance := data.GetInstance(tree + "." + volume + "." + vserver)
-				for _, label := range my.data[attribute].GetExportOptions().GetChildS("instance_keys").GetAllChildContentS() {
-					if value := qtreeInstance.GetLabel(label); value != "" {
-						instance.SetLabel(label, value)
-					}
-				}
-
-			} else {
-				my.Logger.Debug().Msgf("instance without [%s], skipping", attribute)
-			}
-
-			output = append(output, data1)
+		if err != nil {
+			return nil, err
 		}
-	}
 
-	// second loop to populate numeric data
-	for key, quota := range quotas {
+		if result == nil {
+			break
+		}
 
-		tree := quota.GetChildContentS("tree")
-		volume := quota.GetChildContentS("volume")
-		vserver := quota.GetChildContentS("vserver")
+		if x := result.GetChildS("attributes-list"); x != nil {
+			quotas = x.GetChildren()
+		}
 
-		for attribute, data1 := range my.data {
+		if len(quotas) == 0 {
+			return nil, errors.New(errors.ERR_NO_INSTANCE, "no quota instances found")
+		}
 
-			objectElem := quota.GetChildS(attribute)
-			if objectElem == nil {
-				continue
+		my.Logger.Debug().Msgf("fetching %d quota counters", len(quotas))
+
+		for quotaIndex, quota := range quotas {
+
+			tree := quota.GetChildContentS("tree")
+			volume := quota.GetChildContentS("volume")
+			vserver := quota.GetChildContentS("vserver")
+
+			for attribute, _ := range my.data.GetMetrics() {
+
+				objectElem := quota.GetChildS(attribute)
+				if objectElem == nil {
+					my.Logger.Warn().Msgf("no [%s] instances on this %s.%s.%s", attribute, vserver, volume, tree)
+					continue
+				}
+
+				if ok := quota.GetChildContentS(attribute); ok != "" {
+					// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
+					instanceKey := vserver + "." + volume + "." + tree + "." + strconv.Itoa(quotaIndex) + "." + attribute
+					instance, err := my.data.NewInstance(instanceKey)
+
+					if err != nil {
+						my.Logger.Debug().Msgf("add (%s) instance: %v", attribute, err)
+						return nil, err
+					}
+
+					my.Logger.Debug().Msgf("add (%s) instance: %s.%s.%s", attribute, vserver, volume, tree)
+
+					qtreeInstance := data.GetInstance(tree + "." + volume + "." + vserver)
+					for _, label := range my.data.GetExportOptions().GetChildS("instance_keys").GetAllChildContentS() {
+						if value := qtreeInstance.GetLabel(label); value != "" {
+							instance.SetLabel(label, value)
+						}
+					}
+
+				} else {
+					my.Logger.Debug().Msgf("instance without [%s], skipping", attribute)
+				}
+
+				output = append(output, my.data)
 			}
+		}
 
-			instance := data1.GetInstance(vserver + "." + volume + "." + tree + "." + strconv.Itoa(key))
+		//second loop to populate numeric data
+		for key, quota := range quotas {
 
-			if instance == nil {
-				my.Logger.Debug().Msgf("(%s) instance [%s.%s] not found in cache skipping", attribute, volume, vserver)
-				continue
-			}
+			tree := quota.GetChildContentS("tree")
+			volume := quota.GetChildContentS("volume")
+			vserver := quota.GetChildContentS("vserver")
 
-			for metricKey, m := range data1.GetMetrics() {
+			for attribute, m := range my.data.GetMetrics() {
 
-				if value := strings.Split(quota.GetChildContentS(metricKey), " ")[0]; value != "" {
+				objectElem := quota.GetChildS(attribute)
+				if objectElem == nil {
+					continue
+				}
+
+				// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
+				instance := my.data.GetInstance(vserver + "." + volume + "." + tree + "." + strconv.Itoa(key) + "." + attribute)
+
+				if instance == nil {
+					my.Logger.Debug().Msgf("(%s) instance [%s.%s.%s] not found in cache skipping", attribute, vserver, volume, tree)
+					continue
+				}
+
+				if value := strings.Split(quota.GetChildContentS(attribute), " ")[0]; value != "" {
 					// Few quota metrics would have value '-' which means unlimited (ex: disk-limit)
 					if value == "-" {
-						value = strconv.FormatFloat(math.MaxFloat64, 'E', -1, 64)
+						value = "0"
 					}
-					my.Logger.Info().Msgf("vals %s (%s) ", metricKey, value)
 					if err := m.SetValueString(instance, value); err != nil {
-						my.Logger.Debug().Msgf("(%s) failed to parse value (%s): %v", metricKey, value, err)
+						my.Logger.Debug().Msgf("(%s) failed to parse value (%s): %v", attribute, value, err)
 					} else {
-						my.Logger.Debug().Msgf("(%s) added value (%s)", metricKey, value)
+						my.Logger.Debug().Msgf("(%s) added value (%s)", attribute, value)
 					}
 				}
 			}
