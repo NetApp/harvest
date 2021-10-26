1ec8bff43a2761a1e92aa2b4b4b454ad2c2e66b2
cdurai-netapp <cdurai@netapp.com>
2021-10-26 19:16:31 -0400

    fix: add metadata counter

--- integration/test/data/counter_data.go
+++ integration/test/data/counter_data.go
@@ -16,7 +16,7 @@ func GetCounterMap() map[string][]string {
 		"qos_detail_volume_resource_latency", "qos_detail_resource_latency"}
 	counterMap["NO_DATA_CONTAINS"] = []string{"nvme_lif", "flashpool", "fcp", "snapmirror_", "node_nfs"}
 	//if docker.IsDockerBasedPoller() || setup.IsMac {
-	counterMap["NO_DATA_CONTAINS"] = append(counterMap["NO_DATA_CONTAINS"], "poller")
+	counterMap["NO_DATA_CONTAINS"] = append(counterMap["NO_DATA_CONTAINS"], "poller", "metadata_exporter_count")
 	//}
 	return counterMap
 }
