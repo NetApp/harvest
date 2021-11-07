ca3f42dec419f13626ed9a03022ae5508cbbdab7
Chris Grindstaff <chris@gstaff.org>
2021-11-07 07:57:11 -0500

    feat: improve instantiated systemd template (#644)
    * feat: improve instantiated systemd template
    
    and docs
    Fixes #643
    
    * feat: improve instantiated systemd template
    Verified with config that contains:
    - no pollers that have a Unix collector
    - single poller that has a Unix collector
    - multiple pollers with a Unix collector
    - multiple pollers with a multiple collectors, one of which is a Unix collector

--- cmd/tools/generate/generate.go
+++ cmd/tools/generate/generate.go
@@ -185,7 +185,36 @@ func generateSystemd(path string) {
 	println("Save the following to " + color.Colorize("/etc/systemd/system/harvest.target", color.Green) +
 		" or " + color.Colorize("| sudo tee /etc/systemd/system/harvest.target", color.Green))
 	println("and then run " + color.Colorize("systemctl daemon-reload", color.Green))
-	err = t.Execute(os.Stdout, conf.Config)
+	// reorder list of pollers so that unix collectors are last, see https://github.com/NetApp/harvest/issues/643
+	pollers := make([]string, 0)
+	unixPollers := make([]string, 0)
+	for _, k := range conf.Config.PollersOrdered {
+		pollers = append(pollers, k)
+	}
+	// iterate over the pollers backwards, so we don't skip any when removing
+	for i := len(pollers) - 1; i >= 0; i-- {
+		pollerName := pollers[i]
+		poller, ok := conf.Config.Pollers[pollerName]
+		if !ok || poller == nil {
+			continue
+		}
+		// if unix is in the poller's list of collectors, remove it from the list of pollers
+		for _, c := range poller.Collectors {
+			if c.Name == "Unix" {
+				pollers = append(pollers[:i], pollers[i+1:]...)
+				unixPollers = append(unixPollers, pollerName)
+				break
+			}
+		}
+	}
+	for _, poller := range unixPollers {
+		pollers = append(pollers, poller)
+	}
+	err = t.Execute(os.Stdout, struct {
+		PollersOrdered []string
+	}{
+		PollersOrdered: pollers,
+	})
 	if err != nil {
 		panic(err)
 	}
