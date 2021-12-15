ff1e86eb1ba70522228c22768880bf08e9c5f45c
cdurai-netapp <cdurai@netapp.com>
2021-12-15 02:22:04 -0500

    ci: fix format error

--- integration/test/data/counter_data.go
+++ integration/test/data/counter_data.go
@@ -15,7 +15,7 @@ func GetCounterMap() map[string][]string {
 		"svm_write_total", "node_cifs_signed_sessions", "metadata_target_ping", "snapmirror_labels",
 		"qos_detail_volume_resource_latency", "qos_detail_resource_latency"}
 	counterMap["NO_DATA_CONTAINS"] = []string{"nvme_lif", "flashpool", "fcp", "snapmirror_", "node_nfs",
-		"fcvi" , "path_"}
+		"fcvi", "path_"}
 	//if docker.IsDockerBasedPoller() || setup.IsMac {
 	counterMap["NO_DATA_CONTAINS"] = append(counterMap["NO_DATA_CONTAINS"], "poller", "metadata_exporter_count")
 	//}
