3b4e596c9c2235745e4f59cb909102be2f12676f
Hardik Leuva <hardikl@netapp.com>
2022-01-05 16:16:06 +0530

    feat: adding qtree template for 7mode

--- cmd/collectors/zapi/plugins/quota/quota.go
+++ cmd/collectors/zapi/plugins/quota/quota.go
@@ -49,7 +49,11 @@ func (my *Quota) Init() error {
 		return err
 	}
 
-	my.query = "quota-report-iter"
+	if my.client.IsClustered() {
+		my.query = "quota-report-iter"
+	} else {
+		my.query = "quota-report"
+	}
 	my.Logger.Debug().Msg("plugin connected!")
 
 	my.data = matrix.New(my.Parent+".Qtree", "qtree", "qtree")
@@ -107,15 +111,7 @@ func (my *Quota) Init() error {
 }
 
 func (my *Quota) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
-
-	var (
-		request, result *node.Node
-		quotas          []*node.Node
-		tag             string
-		err             error
-	)
-
-	var output []*matrix.Matrix
+	var request *node.Node
 
 	// Purge and reset data
 	my.data.PurgeInstances()
@@ -125,11 +121,27 @@ func (my *Quota) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 	my.data.SetGlobalLabels(data.GetGlobalLabels())
 
 	request = node.NewXmlS(my.query)
+
+	if my.client.IsClustered() {
+		return my.handleCMode(request, data)
+	} else {
+		return my.handle7Mode(request, data)
+	}
+}
+
+func (my *Quota) handleCMode(request *node.Node, data *matrix.Matrix) ([]*matrix.Matrix, error) {
+	var (
+		result *node.Node
+		quotas []*node.Node
+		output []*matrix.Matrix
+		err    error
+	)
+
 	if my.client.IsClustered() && my.batchSize != "" {
 		request.NewChildS("max-records", my.batchSize)
 	}
 
-	tag = "initial"
+	tag := "initial"
 
 	for {
 		result, tag, err = my.client.InvokeBatchRequest(request, tag)
@@ -150,7 +162,7 @@ func (my *Quota) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 			return nil, errors.New(errors.ERR_NO_INSTANCE, "no quota instances found")
 		}
 
-		my.Logger.Debug().Msgf("fetching %d quota counters", len(quotas))
+		my.Logger.Debug().Msgf("fetching %d quotas", len(quotas))
 
 		for quotaIndex, quota := range quotas {
 
@@ -220,5 +232,94 @@ func (my *Quota) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 		}
 
 	}
+	return output, nil
+}
+
+func (my *Quota) handle7Mode(request *node.Node, data *matrix.Matrix) ([]*matrix.Matrix, error) {
+	var (
+		result *node.Node
+		output []*matrix.Matrix
+		err    error
+	)
+
+	if result, err = my.client.InvokeRequest(request); err != nil {
+		return nil, err
+	}
+
+	quotas := result.SearchChildren([]string{"quota"})
+
+	if len(quotas) == 0 {
+		return nil, errors.New(errors.ERR_NO_INSTANCE, "no quota instances found")
+	}
+
+	my.Logger.Debug().Msgf("fetching %d quotas", len(quotas))
+
+	for quotaIndex, quota := range quotas {
+
+		tree := quota.GetChildContentS("tree")
+		volume := quota.GetChildContentS("volume")
+		vserver := quota.GetChildContentS("vfiler")
+
+		// If quota-type is not a Qtree, then skip
+		if quota.GetChildContentS("quota-type") != "tree" {
+			continue
+		}
+
+		for attribute, m := range my.data.GetMetrics() {
+
+			objectElem := quota.GetChildS(attribute)
+			if objectElem == nil {
+				my.Logger.Warn().Msgf("no [%s] instances on this %s.%s.%s", attribute, vserver, volume, tree)
+				continue
+			}
+
+			if attrValue := quota.GetChildContentS(attribute); attrValue != "" {
+				qtreeInstance := data.GetInstance(tree + "." + volume + "." + vserver)
+				if !qtreeInstance.IsExportable() {
+					continue
+				}
+				// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
+				instanceKey := vserver + "." + volume + "." + tree + "." + strconv.Itoa(quotaIndex) + "." + attribute
+				instance, err := my.data.NewInstance(instanceKey)
+
+				if err != nil {
+					my.Logger.Debug().Msgf("add (%s) instance: %v", attribute, err)
+					return nil, err
+				}
+
+				my.Logger.Debug().Msgf("add (%s) instance: %s.%s.%s", attribute, vserver, volume, tree)
+
+				for _, label := range my.data.GetExportOptions().GetChildS("instance_keys").GetAllChildContentS() {
+					if value := qtreeInstance.GetLabel(label); value != "" {
+						instance.SetLabel(label, value)
+					}
+				}
+
+				// If the Qtree is the volume itself, than qtree label is empty, so copy the volume name to qtree.
+				if tree == "" {
+					instance.SetLabel("qtree", volume)
+				}
+
+				// populate numeric data
+				if value := strings.Split(attrValue, " ")[0]; value != "" {
+					// Few quota metrics would have value '-' which means unlimited (ex: disk-limit)
+					if value == "-" {
+						value = "0"
+					}
+					if err := m.SetValueString(instance, value); err != nil {
+						my.Logger.Debug().Msgf("(%s) failed to parse value (%s): %v", attribute, value, err)
+					} else {
+						my.Logger.Debug().Msgf("(%s) added value (%s)", attribute, value)
+					}
+				}
+
+			} else {
+				my.Logger.Debug().Msgf("instance without [%s], skipping", attribute)
+			}
+
+			output = append(output, my.data)
+		}
+	}
+
 	return output, nil
 }
