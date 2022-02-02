bb9afa451e6f7c522177345c2976d1d9ec513069
Rahul Gupta <rahul.gupta@netapp.com>
2022-02-02 23:35:26 +0530

    feat: grafana dashboards should match release version
    Fixes #631

--- integration/test/test.sh
+++ integration/test/test.sh
@@ -4,5 +4,12 @@ echo "Dir : $SCRIPT_DIR"
 cd $SCRIPT_DIR/test
 tag=${1?Specify valid test tag}
 export PATH=$PATH:/usr/local/go/bin
+if [ ! -n "$VERSION" ]; then
+  echo "VERSION not supplied."
+  VERSION="$(date +%Y.%m.%d%H | cut -c 3-)"
+fi
+
+LD_FLAGS="-X 'goharvest2/cmd/harvest/version.VERSION=$VERSION'"
+
 go mod tidy
-go test -timeout 30m -tags=$tag
+go test -timeout 30m -tags=$tag -ldflags=$(LD_FLAGS)
