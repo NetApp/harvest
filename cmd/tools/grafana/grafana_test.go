15ba61ff8589c9b85476cab726ba9709baa6e0f7
Chris Grindstaff <chris@gstaff.org>
2022-02-04 14:38:59 -0500

    fix: relax Grafana version check constraint
    Fixes #828

--- cmd/tools/grafana/grafana_test.go
+++ cmd/tools/grafana/grafana_test.go
@@ -7,9 +7,8 @@ import (
 
 func TestCheckVersion(t *testing.T) {
 
-	inputVersion := []string{"7.2.3.4", "abc.1.3", "4.5.4", "7.1.0", "7.5.5"}
-	expectedOutPut := []bool{true, false, false, true, true}
-	// version length greater than 3
+	inputVersion := []string{"8.4.0-beta1", "7.2.3.4", "abc.1.3", "4.5.4", "7.1.0", "7.5.5"}
+	expectedOutPut := []bool{true, true, false, false, true, true}
 
 	for i, s := range inputVersion {
 		c := checkVersion(s)
