c3762adc871888a144517f90e9cabd25dfb75b08
Chris Grindstaff <chris@gstaff.org>
2022-02-08 10:13:27 -0500

    fix: harvest status should show pollers exporting to InfluxDB

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -124,7 +124,7 @@ func GetPids(search string) ([]int32, error) {
 	return result, err
 }
 
-var pidAndPollerRegex = regexp.MustCompile(`(\d+).*?poller.*?--poller (.*?) .*?--promPort (\d+)`)
+var pidAndPollerRegex = regexp.MustCompile(`(\d+).*?poller.*?--poller (.*?)\s`)
 var profRegex = regexp.MustCompile(`--profiling (\d+)`)
 var promRegex = regexp.MustCompile(`--promPort (\d+)`)
 
