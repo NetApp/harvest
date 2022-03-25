940310c74803180834c230f52c2c68488ee60a9b
Chris Grindstaff <chris@gstaff.org>
2022-03-25 12:53:25 -0400

    ci: ignore nic_ counters

--- integration/test/data/counter_data.go
+++ integration/test/data/counter_data.go
@@ -11,11 +11,30 @@ const PrometheusUrl string = "http://localhost:9090"
 
 func GetCounterMap() map[string][]string {
 	counterMap := make(map[string][]string)
-	counterMap["NO_DATA_EXACT"] = []string{"fcp_util_percent", "nic_new_status", "svm_read_total",
-		"svm_write_total", "node_cifs_signed_sessions", "metadata_target_ping", "snapmirror_labels",
-		"qos_detail_volume_resource_latency", "qos_detail_resource_latency"}
-	counterMap["NO_DATA_CONTAINS"] = []string{"nvme_lif", "flashpool", "fcp", "snapmirror_", "node_nfs",
-		"fcvi", "path_", "node_cifs_", "flashcache_", "svm_cifs_"}
+	counterMap["NO_DATA_EXACT"] = []string{
+		"fcp_util_percent",
+		"metadata_target_ping",
+		"nic_new_status",
+		"node_cifs_signed_sessions",
+		"qos_detail_resource_latency",
+		"qos_detail_volume_resource_latency",
+		"snapmirror_labels",
+		"svm_read_total",
+		"svm_write_total",
+	}
+	counterMap["NO_DATA_CONTAINS"] = []string{
+		"fcp",
+		"fcvi",
+		"flashcache_",
+		"flashpool",
+		"nic_",
+		"node_cifs_",
+		"node_nfs",
+		"nvme_lif",
+		"path_",
+		"snapmirror_",
+		"svm_cifs_",
+	}
 	//if docker.IsDockerBasedPoller() || setup.IsMac {
 	counterMap["NO_DATA_CONTAINS"] = append(counterMap["NO_DATA_CONTAINS"], "poller", "metadata_exporter_count")
 	//}
