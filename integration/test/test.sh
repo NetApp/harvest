facd8c2fbd566a325346230279c172a5ae30e0bb
Chris Grindstaff <chris@gstaff.org>
2022-03-30 02:46:40 -0400

    ci: address shellcheck warnings (#934)

--- integration/test/test.sh
+++ integration/test/test.sh
@@ -1,16 +1,16 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT_DIR="$(dirname "$SCRIPT_DIR")"
 echo "Dir : $SCRIPT_DIR"
-cd $SCRIPT_DIR/test
+cd "$SCRIPT_DIR"/test || exit
 tag=${1?Specify valid test tag}
 export PATH=$PATH:/usr/local/go/bin
-if [ ! -n "$VERSION" ]; then
+if [ -z "$VERSION" ]; then
   VERSION="$(date +%Y.%m.%d%H | cut -c 3-)"
   echo "VERSION not supplied, using $VERSION"
 fi
 
 LD_FLAGS="-X ""'""goharvest2/cmd/harvest/version.VERSION=${VERSION}""'"""
-echo $LD_FLAGS
+echo "$LD_FLAGS"
 
 go mod tidy
-go test -timeout 30m -tags=$tag -ldflags="$LD_FLAGS"
+go test -timeout 30m -tags="$tag" -ldflags="$LD_FLAGS"
