3d3ccce862c7ff5db04f79bb145a8e9d75f45ac4
Hardik Leuva <hardikl@netapp.com>
2022-04-04 20:11:56 +0530

    feat: security dashboard - global var with version

--- cmd/collectors/rest/plugins/svm/svm.go
+++ cmd/collectors/rest/plugins/svm/svm.go
@@ -5,10 +5,14 @@
 package svm
 
 import (
+	"github.com/hashicorp/go-version"
 	"goharvest2/cmd/poller/plugin"
 	"goharvest2/pkg/matrix"
+	"strconv"
 	"strings"
 )
+
+const Version_9_10 = "9.10.0"
 
 type SVM struct {
 	*plugin.AbstractPlugin
@@ -24,6 +28,8 @@ func (my *SVM) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 }
 
 func (my *SVM) setNameservice(data *matrix.Matrix) {
+	isHigher, _ := my.isHigherThan9_10(data.GetGlobalLabels().Get("clusterversion"))
+
 	for _, instance := range data.GetInstances() {
 		requiredNSDb := false
 		requiredNSSource := false
@@ -42,6 +48,26 @@ func (my *SVM) setNameservice(data *matrix.Matrix) {
 		} else {
 			instance.SetLabel("nis_authentication_enabled", "false")
 		}
+
+		instance.SetLabel("isHigherThan9_10", strconv.FormatBool(isHigher))
 	}
 
+}
+
+func (my *SVM) isHigherThan9_10(current string) (bool, error) {
+	my.Logger.Info().Str("Version", current).Msg("cluster")
+
+	versionToCompare, err := version.NewVersion(Version_9_10)
+	if err != nil {
+		return false, err
+	}
+	currentVersion, err := version.NewVersion(current)
+	if err != nil {
+		return false, err
+	}
+	if currentVersion.GreaterThan(versionToCompare) {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
