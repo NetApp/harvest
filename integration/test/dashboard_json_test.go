142f2a08fd8fc52a51adda14f7a40869c967975a
Rahul Gupta <rahul.gupta@netapp.com>
2022-08-04 13:40:33 +0530

    feat: ignore headroom dashboard in CI

--- integration/test/dashboard_json_test.go
+++ integration/test/dashboard_json_test.go
@@ -164,7 +164,8 @@ func (suite *DashboardJsonTestSuite) TestJsonExpression() {
 }
 
 func ShouldSkipDashboard(path string) bool {
-	skip := []string{"nfs4storePool_detail"}
+	//ignore headroom dashboard from CI as it uses dynamic variables in query
+	skip := []string{"nfs4storePool_detail", "headroom"}
 	for _, s := range skip {
 		if strings.Contains(path, s) {
 			return true
