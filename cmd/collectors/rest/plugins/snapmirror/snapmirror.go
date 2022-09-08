b845a8abade6090c932beb5960102687adca0728
hardikl <hardikl@netapp.com>
2022-09-08 18:49:16 +0530

    fix: snapmirror relationships from source side(server)

--- cmd/collectors/rest/plugins/snapmirror/snapmirror.go
+++ cmd/collectors/rest/plugins/snapmirror/snapmirror.go
@@ -10,6 +10,7 @@ import (
 	"github.com/netapp/harvest/v2/pkg/conf"
 	"github.com/netapp/harvest/v2/pkg/matrix"
 	"github.com/tidwall/gjson"
+	"strings"
 	"time"
 )
 
@@ -21,6 +22,7 @@ type SnapMirror struct {
 	query          string
 	nodeUpdCounter int
 	svmVolToNode   map[string]string
+	svmPeerDataMap map[string]string // [peer SVM alias name] -> [peer SVM actual name] - [peer cluster name] map
 }
 
 func New(p *plugin.AbstractPlugin) plugin.Plugin {
@@ -47,6 +49,7 @@ func (my *SnapMirror) Init() error {
 
 	my.query = "api/private/cli/volume"
 	my.svmVolToNode = make(map[string]string)
+	my.svmPeerDataMap = make(map[string]string)
 
 	// Assigned the value to nodeUpdCounter so that plugin would be invoked first time to populate cache.
 	my.nodeUpdCounter = PluginInvocationRate
@@ -62,6 +65,12 @@ func (my *SnapMirror) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 			return nil, err
 		}
 		my.Logger.Debug().Msg("updated node cache")
+
+		cluster, _ := data.GetGlobalLabels().GetHas("cluster")
+		if err := my.getSVMPeerData(cluster); err != nil {
+			return nil, err
+		}
+		my.Logger.Debug().Msg("updated svm peer")
 	}
 
 	// update volume instance labels
@@ -99,7 +108,35 @@ func (my *SnapMirror) updateNodeCache() error {
 	return nil
 }
 
+func (my *SnapMirror) getSVMPeerData(cluster string) error {
+	// Clean svmPeerMap map
+	my.svmPeerDataMap = make(map[string]string)
+	fields := []string{"name", "peer.svm.name", "peer.cluster.name"}
+	query := "api/svm/peers"
+	href := rest.BuildHref("", strings.Join(fields, ","), []string{"peer.cluster.name=!" + cluster}, "", "", "", "", query)
+
+	result, err := rest.Fetch(my.client, href)
+	if err != nil {
+		my.Logger.Error().Err(err).Str("href", href).Msg("Failed to fetch data")
+		return err
+	}
+
+	if len(result) == 0 {
+		my.Logger.Debug().Msg("No svm peer found")
+		return nil
+	}
+
+	for _, peerData := range result {
+		localSvmName := peerData.Get("name").String()
+		actualSvmName := peerData.Get("peer.svm.name").String()
+		peerClusterName := peerData.Get("peer.cluster.name").String()
+		my.svmPeerDataMap[localSvmName] = actualSvmName + ":" + peerClusterName
+	}
+	return nil
+}
+
 func (my *SnapMirror) updateSMLabels(data *matrix.Matrix) {
+	cluster, _ := data.GetGlobalLabels().GetHas("cluster")
 	for _, instance := range data.GetInstances() {
 		volumeName := instance.GetLabel("source_volume")
 		vserverName := instance.GetLabel("source_vserver")
@@ -107,6 +144,17 @@ func (my *SnapMirror) updateSMLabels(data *matrix.Matrix) {
 		// Update source_node label in snapmirror
 		if node, ok := my.svmVolToNode[vserverName+volumeName]; ok {
 			instance.SetLabel("source_node", node)
+		}
+
+		// Update source_vserver in snapmirror (In case of inter-cluster SM- vserver name may differ)
+		if peerDetail, ok := my.svmPeerDataMap[vserverName]; ok {
+			peerData := strings.Split(peerDetail, ":")
+			instance.SetLabel("source_vserver", peerData[0])
+			instance.SetLabel("source_cluster", peerData[1])
+		}
+
+		if sourceCluster := instance.GetLabel("source_cluster"); sourceCluster == "" {
+			instance.SetLabel("source_cluster", cluster)
 		}
 
 		// update the protectedBy and protectionSourceType fields and derivedRelationshipType in snapmirror_labels
