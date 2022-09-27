6a076c169bb706c63a9b2950ebf1947a74041c75
Rahul Gupta <rahul.gupta@netapp.com>
2022-09-27 20:59:56 +0530

    fix: remove duplicate network dashboards
    Fixes #1306

--- cmd/collectors/restperf/plugins/nic/nic.go
+++ cmd/collectors/restperf/plugins/nic/nic.go
@@ -72,47 +72,62 @@ func (me *Nic) Run(data *matrix.Matrix) ([]*matrix.Matrix, error) {
 		var s string
 		var err error
 
+		if s != "" {
+			if s = instance.GetLabel("speed"); strings.HasSuffix(s, "M") {
+				base, err = strconv.Atoi(strings.TrimSuffix(s, "M"))
+				if err != nil {
+					me.Logger.Warn().Msgf("convert speed [%s]", s)
+				} else {
+					// NIC speed value converted from Mbps to Bps(bytes per second)
+					speed = base * 125000
+					me.Logger.Debug().Msgf("converted speed (%s) to numeric (%d)", s, speed)
+				}
+			} else if speed, err = strconv.Atoi(s); err != nil {
+				me.Logger.Warn().Msgf("convert speed [%s]", s)
+			}
+
+			if speed != 0 {
+
+				var rxBytes, txBytes, rxPercent, txPercent float64
+				var rxOk, txOk, passRx, passTx bool
+
+				if rxBytes, rxOk, passRx = read.GetValueFloat64(instance); rxOk && passRx {
+					rxPercent = rxBytes / float64(speed)
+					err := rx.SetValueFloat64(instance, rxPercent)
+					if err != nil {
+						me.Logger.Error().Stack().Err(err).Msg("error")
+					}
+				}
+
+				if txBytes, txOk, passTx = write.GetValueFloat64(instance); txOk && passTx {
+					txPercent = txBytes / float64(speed)
+					err := tx.SetValueFloat64(instance, txPercent)
+					if err != nil {
+						me.Logger.Error().Stack().Err(err).Msg("error")
+					}
+				}
+
+				if (rxOk || txOk) && (passRx || passTx) {
+					err := util.SetValueFloat64(instance, math.Max(rxPercent, txPercent))
+					if err != nil {
+						me.Logger.Error().Stack().Err(err).Msg("error")
+					}
+				}
+			}
+		}
+
 		if s = instance.GetLabel("speed"); strings.HasSuffix(s, "M") {
 			base, err = strconv.Atoi(strings.TrimSuffix(s, "M"))
 			if err != nil {
 				me.Logger.Warn().Msgf("convert speed [%s]", s)
 			} else {
-				// NIC speed value converted from Mbps to Bps(bytes per second)
-				speed = base * 125000
+				// NIC speed value converted from Mbps to bps(bits per second)
+				speed = base * 1_000_000
 				instance.SetLabel("speed", strconv.Itoa(speed))
 				me.Logger.Debug().Msgf("converted speed (%s) to numeric (%d)", s, speed)
 			}
 		} else if speed, err = strconv.Atoi(s); err != nil {
 			me.Logger.Warn().Msgf("convert speed [%s]", s)
-		}
-
-		if speed != 0 {
-
-			var rxBytes, txBytes, rxPercent, txPercent float64
-			var rxOk, txOk, passRx, passTx bool
-
-			if rxBytes, rxOk, passRx = read.GetValueFloat64(instance); rxOk && passRx {
-				rxPercent = rxBytes / float64(speed)
-				err := rx.SetValueFloat64(instance, rxPercent)
-				if err != nil {
-					me.Logger.Error().Stack().Err(err).Msg("error")
-				}
-			}
-
-			if txBytes, txOk, passTx = write.GetValueFloat64(instance); txOk && passTx {
-				txPercent = txBytes / float64(speed)
-				err := tx.SetValueFloat64(instance, txPercent)
-				if err != nil {
-					me.Logger.Error().Stack().Err(err).Msg("error")
-				}
-			}
-
-			if (rxOk || txOk) && (passRx || passTx) {
-				err := util.SetValueFloat64(instance, math.Max(rxPercent, txPercent))
-				if err != nil {
-					me.Logger.Error().Stack().Err(err).Msg("error")
-				}
-			}
 		}
 
 		// truncate redundant prefix in nic type
