1b6c7150d90b7589914792b7c51dc9a45fb893d2
Chris Grindstaff <chris@gstaff.org>
2022-10-28 12:01:07 -0400

    test: use table test for metric_float64

--- pkg/matrix/metric_float64_test.go
+++ pkg/matrix/metric_float64_test.go
@@ -2,6 +2,7 @@ package matrix
 
 import (
 	"github.com/netapp/harvest/v2/pkg/logging"
+	"math"
 	"testing"
 )
 
@@ -24,282 +25,103 @@ func setupMatrix(previousRaw float64, currentRaw float64) (*Matrix, *Matrix) {
 	return m, m1
 }
 
+type test struct {
+	name      string
+	curRaw    float64
+	prevRaw   float64
+	cooked    float64
+	threshold int
+	scalar    uint
+	skips     int
+	passes    bool
+}
+
 func TestMetricFloat64_Delta(t *testing.T) {
-	// When previous == current and previous > 0 and current > 0
-	//      then don't skip and pass is true
-	previous, current := setupMatrix(10, 10)
-	skip, _ := current.GetMetric("speed").Delta(previous.GetMetric("speed"), logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v := current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass := current.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
+	tests := []test{
+		{curRaw: 10, prevRaw: 10, cooked: 0, skips: 0, passes: true, name: "no increase"},
+		{curRaw: 20, prevRaw: 10, cooked: 10, skips: 0, passes: true, name: "normal increase"},
+		{curRaw: 10, prevRaw: 20, cooked: -10, skips: 1, passes: false, name: "bug negative"},
+		{curRaw: 10, prevRaw: 0, cooked: 10, skips: 1, passes: false, name: "bug zeroPrev"},
+		{curRaw: 0, prevRaw: 10, cooked: -10, skips: 1, passes: false, name: "bug zeroCur"},
 	}
 
-	// When previous < current and previous > 0 and current > 0
-	//      then don't skip and pass is true
-	previous, current = setupMatrix(10, 20)
-	skip, _ = current.GetMetric("speed").Delta(previous.GetMetric("speed"), logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v = current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 10 {
-		t.Errorf("expected = %v, got %v", 10, v)
-	}
-	pass = current.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
-	}
-
-	// When previous > current and previous > 0 and current > 0
-	//      then skip and pass is false
-	previous, current = setupMatrix(20, 10)
-	skip, _ = current.GetMetric("speed").Delta(previous.GetMetric("speed"), logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != -10 {
-		t.Errorf("expected = %v, got %v", -10, v)
-	}
-	pass = current.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When previous == 0 current > 0
-	//      then skip and pass is false
-	previous, current = setupMatrix(0, 10)
-	skip, _ = current.GetMetric("speed").Delta(previous.GetMetric("speed"), logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 10 {
-		t.Errorf("expected = %v, got %v", 10, v)
-	}
-	pass = current.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			previous, current := setupMatrix(tt.prevRaw, tt.curRaw)
+			skips, err := current.GetMetric("speed").Delta(previous.GetMetric("speed"), logging.Get())
+			matrixTest(t, tt, current, skips, err)
+		})
 	}
 }
 
 func TestMetricFloat64_Divide(t *testing.T) {
-	// When numerator == denominator and numerator > 0 and denominator > 0
-	//      then don't skip and pass is true
-	numerator, denominator := setupMatrix(20, 10)
-	skip, _ := numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v := numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 2 {
-		t.Errorf("expected = %v, got %v", 2, v)
-	}
-	pass := numerator.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
+	tests := []test{
+		{prevRaw: 20, curRaw: 10, cooked: 2, skips: 0, passes: true, name: "normal"},
+		{prevRaw: -20, curRaw: 10, cooked: -2, skips: 1, passes: false, name: "bug negative num"},
+		{prevRaw: 20, curRaw: -10, cooked: -2, skips: 1, passes: false, name: "bug negative den"},
+		{prevRaw: -20, curRaw: -10, cooked: 2, skips: 1, passes: false, name: "bug negative both"},
+		{prevRaw: 20, curRaw: 0, cooked: 0, skips: 0, passes: true, name: "allow zero den"},
+		{prevRaw: 0, curRaw: 0, cooked: 0, skips: 0, passes: true, name: "allow zero both"},
 	}
 
-	// When numerator < 0 and denominator > 0
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(-20, 10)
-	skip, _ = numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != -2 {
-		t.Errorf("expected = %v, got %v", -2, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When numerator > 0 denominator < 0
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(20, -10)
-	skip, _ = numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != -2 {
-		t.Errorf("expected = %v, got %v", -2, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When numerator < 0 denominator < 0
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(-20, -10)
-	skip, _ = numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 2 {
-		t.Errorf("expected = %v, got %v", 2, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When numerator > 0 denominator == 0
-	//      then don't skip and pass is true
-	numerator, denominator = setupMatrix(20, 0)
-	skip, _ = numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
-	}
-
-	// When numerator == 0 denominator == 0
-	//      then don't skip and pass is true
-	numerator, denominator = setupMatrix(0, 0)
-	skip, _ = numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			numerator, denominator := setupMatrix(tt.prevRaw, tt.curRaw)
+			skips, err := numerator.GetMetric("speed").Divide(denominator.GetMetric("speed"), logging.Get())
+			matrixTest(t, tt, numerator, skips, err)
+		})
 	}
 }
 
 func TestMetricFloat64_DivideWithThreshold(t *testing.T) {
-	// When numerator == denominator and numerator > 0 and denominator > 0 and denominator > threshold
-	//      then don't skip and pass is true
-	numerator, denominator := setupMatrix(20, 10)
-	skip, _ := numerator.GetMetric("speed").DivideWithThreshold(denominator.GetMetric("speed"), 5, logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v := numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 2 {
-		t.Errorf("expected = %v, got %v", 2, v)
-	}
-	pass := numerator.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
+	tests := []test{
+		{prevRaw: 20, curRaw: 10, threshold: 5, cooked: 2, skips: 0, passes: true, name: "normal"},
+		{prevRaw: 20, curRaw: 10, threshold: 15, cooked: 0, skips: 0, passes: true, name: "normal < threshold"},
+		{prevRaw: 20, curRaw: -10, threshold: 5, cooked: 0, skips: 1, passes: false, name: "bug negative den"},
+		{prevRaw: -20, curRaw: -10, threshold: 5, cooked: 0, skips: 1, passes: false, name: "bug negative both"},
+		{prevRaw: 20, curRaw: 0, threshold: 0, cooked: math.Inf(1), skips: 0, passes: true, name: "allow no threshold"},
+		{prevRaw: 0, curRaw: 0, threshold: 5, cooked: 0, skips: 0, passes: true, name: "allow zero both"},
 	}
 
-	// When numerator == denominator and numerator > 0 and denominator > 0 and denominator < threshold
-	//      then don't skip and pass is true
-	numerator, denominator = setupMatrix(20, 10)
-	skip, _ = numerator.GetMetric("speed").DivideWithThreshold(denominator.GetMetric("speed"), 15, logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
-	}
-
-	// When numerator < 0 and denominator > 0 and denominator > threshold
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(-20, 10)
-	skip, _ = numerator.GetMetric("speed").DivideWithThreshold(denominator.GetMetric("speed"), 5, logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != -2 {
-		t.Errorf("expected = %v, got %v", -2, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When numerator > 0 denominator < 0 and denominator < threshold
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(20, -10)
-	skip, _ = numerator.GetMetric("speed").DivideWithThreshold(denominator.GetMetric("speed"), 5, logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
-	}
-
-	// When numerator < 0 denominator < 0 and denominator < threshold
-	//      then skip and pass is false
-	numerator, denominator = setupMatrix(-20, -10)
-	skip, _ = numerator.GetMetric("speed").DivideWithThreshold(denominator.GetMetric("speed"), 5, logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
-	}
-	v = numerator.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 0 {
-		t.Errorf("expected = %v, got %v", 0, v)
-	}
-	pass = numerator.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			numerator, denominator := setupMatrix(tt.prevRaw, tt.curRaw)
+			skips, err := numerator.GetMetric("speed").
+				DivideWithThreshold(denominator.GetMetric("speed"), tt.threshold, logging.Get())
+			matrixTest(t, tt, numerator, skips, err)
+		})
 	}
 }
 
 func TestMetricFloat64_MultiplyByScalar(t *testing.T) {
-	// current > 0
-	//      then don't skip and pass is true
-	_, current := setupMatrix(10, 10)
-	skip, _ := current.GetMetric("speed").MultiplyByScalar(5, logging.Get())
-	if skip != 0 {
-		t.Errorf("expected = %d, got %d", 0, skip)
-	}
-	v := current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != 50 {
-		t.Errorf("expected = %v, got %v", 50, v)
-	}
-	pass := current.GetMetric("speed").GetPass()[0]
-	if !pass {
-		t.Errorf("expected = %t, got %t", pass, !pass)
+	tests := []test{
+		{prevRaw: 10, curRaw: 10, scalar: 5, cooked: 50, skips: 0, passes: true, name: "normal"},
+		{prevRaw: 10, curRaw: -10, scalar: 5, cooked: -50, skips: 1, passes: false, name: "bug negative"},
 	}
 
-	// When current < 0
-	//      then skip and pass is false
-	_, current = setupMatrix(10, -10)
-	skip, _ = current.GetMetric("speed").MultiplyByScalar(5, logging.Get())
-	if skip != 1 {
-		t.Errorf("expected = %d, got %d", 1, skip)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, current := setupMatrix(tt.prevRaw, tt.curRaw)
+			skips, err := current.GetMetric("speed").MultiplyByScalar(tt.scalar, logging.Get())
+			matrixTest(t, tt, current, skips, err)
+		})
 	}
-	v = current.GetMetric("speed").GetValuesFloat64()[0]
-	if v != -50 {
-		t.Errorf("expected = %v, got %v", -50, v)
+}
+
+func matrixTest(t *testing.T, tt test, cur *Matrix, skips int, err error) {
+	if err != nil {
+		t.Error("unexpected error", err)
+		return
 	}
-	pass = current.GetMetric("speed").GetPass()[0]
-	if pass {
-		t.Errorf("expected = %t, got %t", !pass, pass)
+	if skips != tt.skips {
+		t.Errorf("skips expected = %d, got %d", tt.skips, skips)
+	}
+	cooked := cur.GetMetric("speed").GetValuesFloat64()[0]
+	if cooked != tt.cooked {
+		t.Errorf("cooked expected = %v, got %v", tt.cooked, cooked)
+	}
+	passes := cur.GetMetric("speed").GetPass()[0]
+	if passes != tt.passes {
+		t.Errorf("passes expected = %t, got %t", tt.passes, passes)
 	}
 }
