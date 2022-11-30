eddb5b7e918c56b3c4230bae5a4bbe6ee4f8cde8
Rahul Gupta <rahul.gupta@netapp.com>
2022-11-30 22:17:09 +0530

    feat: add test cases

--- pkg/matrix/metric_float64_test.go
+++ pkg/matrix/metric_float64_test.go
@@ -53,20 +53,20 @@ type test struct {
 	name      string
 	curRaw    float64
 	prevRaw   float64
-	cooked    float64
+	cooked    []float64
 	threshold int
 	scalar    uint
 	skips     int
-	record    bool
+	record    []bool
 }
 
 func TestMetricFloat64_Delta(t *testing.T) {
 	tests := []test{
-		{curRaw: 10, prevRaw: 10, cooked: 0, skips: 0, record: true, name: "no increase"},
-		{curRaw: 20, prevRaw: 10, cooked: 10, skips: 0, record: true, name: "normal increase"},
-		{curRaw: 10, prevRaw: 20, cooked: -10, skips: 1, record: false, name: "bug negative"},
-		{curRaw: 10, prevRaw: 0, cooked: 10, skips: 1, record: false, name: "bug zeroPrev"},
-		{curRaw: 0, prevRaw: 10, cooked: -10, skips: 1, record: false, name: "bug zeroCur"},
+		{curRaw: 10, prevRaw: 10, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "no increase SameInstance"},
+		{curRaw: 20, prevRaw: 10, cooked: []float64{10}, skips: 0, record: []bool{true}, name: "normal increase SameInstance"},
+		{curRaw: 10, prevRaw: 20, cooked: []float64{-10}, skips: 1, record: []bool{false}, name: "bug negative SameInstance"},
+		{curRaw: 10, prevRaw: 0, cooked: []float64{10}, skips: 1, record: []bool{false}, name: "bug zeroPrev SameInstance"},
+		{curRaw: 0, prevRaw: 10, cooked: []float64{-10}, skips: 1, record: []bool{false}, name: "bug zeroCur SameInstance"},
 	}
 
 	for _, tt := range tests {
@@ -78,11 +78,11 @@ func TestMetricFloat64_Delta(t *testing.T) {
 	}
 
 	tests1 := []test{
-		{curRaw: 10, prevRaw: 10, cooked: 0, skips: 0, record: true, name: "no increase"},
-		{curRaw: 20, prevRaw: 10, cooked: 10, skips: 0, record: true, name: "normal increase"},
-		{curRaw: 10, prevRaw: 20, cooked: -10, skips: 1, record: false, name: "bug negative"},
-		{curRaw: 10, prevRaw: 0, cooked: 10, skips: 1, record: false, name: "bug zeroPrev"},
-		{curRaw: 0, prevRaw: 10, cooked: -10, skips: 1, record: false, name: "bug zeroCur"},
+		{curRaw: 10, prevRaw: 10, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "no increase DeleteInstance"},
+		{curRaw: 20, prevRaw: 10, cooked: []float64{10}, skips: 0, record: []bool{true}, name: "normal increase DeleteInstance"},
+		{curRaw: 10, prevRaw: 20, cooked: []float64{-10}, skips: 1, record: []bool{false}, name: "bug negative DeleteInstance"},
+		{curRaw: 10, prevRaw: 0, cooked: []float64{10}, skips: 1, record: []bool{false}, name: "bug zeroPrev DeleteInstance"},
+		{curRaw: 0, prevRaw: 10, cooked: []float64{-10}, skips: 1, record: []bool{false}, name: "bug zeroCur DeleteInstance"},
 	}
 
 	for _, tt := range tests1 {
@@ -94,11 +94,11 @@ func TestMetricFloat64_Delta(t *testing.T) {
 	}
 
 	tests2 := []test{
-		{curRaw: 10, prevRaw: 10, cooked: 0, skips: 1, record: true, name: "no increase"},
-		{curRaw: 20, prevRaw: 10, cooked: 10, skips: 1, record: true, name: "normal increase"},
-		{curRaw: 10, prevRaw: 20, cooked: -10, skips: 2, record: false, name: "bug negative"},
-		{curRaw: 10, prevRaw: 0, cooked: 10, skips: 2, record: false, name: "bug zeroPrev"},
-		{curRaw: 0, prevRaw: 10, cooked: -10, skips: 2, record: false, name: "bug zeroCur"},
+		{curRaw: 10, prevRaw: 10, cooked: []float64{0, 10}, skips: 1, record: []bool{true, false}, name: "no increase AddInstance"},
+		{curRaw: 20, prevRaw: 10, cooked: []float64{10, 20}, skips: 1, record: []bool{true, false}, name: "normal increase AddInstance"},
+		{curRaw: 10, prevRaw: 20, cooked: []float64{-10, 10}, skips: 2, record: []bool{false, false}, name: "bug negative AddInstance"},
+		{curRaw: 10, prevRaw: 0, cooked: []float64{10, 10}, skips: 2, record: []bool{false, false}, name: "bug zeroPrev AddInstance"},
+		{curRaw: 0, prevRaw: 10, cooked: []float64{-10, 0}, skips: 2, record: []bool{false, false}, name: "bug zeroCur AddInstance"},
 	}
 
 	for _, tt := range tests2 {
@@ -110,11 +110,11 @@ func TestMetricFloat64_Delta(t *testing.T) {
 	}
 
 	tests3 := []test{
-		{curRaw: 10, prevRaw: 10, cooked: 10, skips: 1, record: false, name: "no increase"},
-		{curRaw: 20, prevRaw: 10, cooked: 20, skips: 1, record: false, name: "normal increase"},
-		{curRaw: 10, prevRaw: 20, cooked: 10, skips: 1, record: false, name: "bug negative"},
-		{curRaw: 10, prevRaw: 0, cooked: 10, skips: 1, record: false, name: "bug zeroPrev"},
-		{curRaw: 0, prevRaw: 10, cooked: 0, skips: 1, record: false, name: "bug zeroCur"},
+		{curRaw: 10, prevRaw: 10, cooked: []float64{10}, skips: 1, record: []bool{false}, name: "no increase AddDeleteInstance"},
+		{curRaw: 20, prevRaw: 10, cooked: []float64{20}, skips: 1, record: []bool{false}, name: "normal increase AddDeleteInstance"},
+		{curRaw: 10, prevRaw: 20, cooked: []float64{10}, skips: 1, record: []bool{false}, name: "bug negative AddDeleteInstance"},
+		{curRaw: 10, prevRaw: 0, cooked: []float64{10}, skips: 1, record: []bool{false}, name: "bug zeroPrev AddDeleteInstance"},
+		{curRaw: 0, prevRaw: 10, cooked: []float64{0}, skips: 1, record: []bool{false}, name: "bug zeroCur AddDeleteInstance"},
 	}
 
 	for _, tt := range tests3 {
@@ -128,12 +128,12 @@ func TestMetricFloat64_Delta(t *testing.T) {
 
 func TestMetricFloat64_Divide(t *testing.T) {
 	tests := []test{
-		{prevRaw: 20, curRaw: 10, cooked: 2, skips: 0, record: true, name: "normal"},
-		{prevRaw: -20, curRaw: 10, cooked: -2, skips: 1, record: false, name: "bug negative num"},
-		{prevRaw: 20, curRaw: -10, cooked: -2, skips: 1, record: false, name: "bug negative den"},
-		{prevRaw: -20, curRaw: -10, cooked: 2, skips: 1, record: false, name: "bug negative both"},
-		{prevRaw: 20, curRaw: 0, cooked: 0, skips: 0, record: true, name: "allow zero den"},
-		{prevRaw: 0, curRaw: 0, cooked: 0, skips: 0, record: true, name: "allow zero both"},
+		{prevRaw: 20, curRaw: 10, cooked: []float64{2}, skips: 0, record: []bool{true}, name: "normal"},
+		{prevRaw: -20, curRaw: 10, cooked: []float64{-2}, skips: 1, record: []bool{false}, name: "bug negative num"},
+		{prevRaw: 20, curRaw: -10, cooked: []float64{-2}, skips: 1, record: []bool{false}, name: "bug negative den"},
+		{prevRaw: -20, curRaw: -10, cooked: []float64{2}, skips: 1, record: []bool{false}, name: "bug negative both"},
+		{prevRaw: 20, curRaw: 0, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "allow zero den"},
+		{prevRaw: 0, curRaw: 0, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "allow zero both"},
 	}
 
 	for _, tt := range tests {
@@ -147,12 +147,12 @@ func TestMetricFloat64_Divide(t *testing.T) {
 
 func TestMetricFloat64_DivideWithThreshold(t *testing.T) {
 	tests := []test{
-		{prevRaw: 20, curRaw: 10, threshold: 5, cooked: 2, skips: 0, record: true, name: "normal"},
-		{prevRaw: 20, curRaw: 10, threshold: 15, cooked: 0, skips: 0, record: true, name: "normal < threshold"},
-		{prevRaw: 20, curRaw: -10, threshold: 5, cooked: 0, skips: 1, record: false, name: "bug negative den"},
-		{prevRaw: -20, curRaw: -10, threshold: 5, cooked: 0, skips: 1, record: false, name: "bug negative both"},
-		{prevRaw: 20, curRaw: 0, threshold: 0, cooked: math.Inf(1), skips: 0, record: true, name: "allow no threshold"},
-		{prevRaw: 0, curRaw: 0, threshold: 5, cooked: 0, skips: 0, record: true, name: "allow zero both"},
+		{prevRaw: 20, curRaw: 10, threshold: 5, cooked: []float64{2}, skips: 0, record: []bool{true}, name: "normal"},
+		{prevRaw: 20, curRaw: 10, threshold: 15, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "normal < threshold"},
+		{prevRaw: 20, curRaw: -10, threshold: 5, cooked: []float64{0}, skips: 1, record: []bool{false}, name: "bug negative den"},
+		{prevRaw: -20, curRaw: -10, threshold: 5, cooked: []float64{0}, skips: 1, record: []bool{false}, name: "bug negative both"},
+		{prevRaw: 20, curRaw: 0, threshold: 0, cooked: []float64{math.Inf(1)}, skips: 0, record: []bool{true}, name: "allow no threshold"},
+		{prevRaw: 0, curRaw: 0, threshold: 5, cooked: []float64{0}, skips: 0, record: []bool{true}, name: "allow zero both"},
 	}
 
 	for _, tt := range tests {
@@ -167,8 +167,8 @@ func TestMetricFloat64_DivideWithThreshold(t *testing.T) {
 
 func TestMetricFloat64_MultiplyByScalar(t *testing.T) {
 	tests := []test{
-		{prevRaw: 10, curRaw: 10, scalar: 5, cooked: 50, skips: 0, record: true, name: "normal"},
-		{prevRaw: 10, curRaw: -10, scalar: 5, cooked: -50, skips: 1, record: false, name: "bug negative"},
+		{prevRaw: 10, curRaw: 10, scalar: 5, cooked: []float64{50}, skips: 0, record: []bool{true}, name: "normal"},
+		{prevRaw: 10, curRaw: -10, scalar: 5, cooked: []float64{-50}, skips: 1, record: []bool{false}, name: "bug negative"},
 	}
 
 	for _, tt := range tests {
@@ -188,13 +188,18 @@ func matrixTest(t *testing.T, tt test, cur *Matrix, skips int, err error) {
 	if skips != tt.skips {
 		t.Errorf("skips expected = %d, got %d", tt.skips, skips)
 	}
-	cooked := cur.GetMetric("speed").GetValuesFloat64()[0]
-	if cooked != tt.cooked {
-		t.Errorf("cooked expected = %v, got %v", tt.cooked, cooked)
+	cooked := cur.GetMetric("speed").GetValuesFloat64()
+	for i := range cooked {
+		if cooked[i] != tt.cooked[i] {
+			t.Errorf("cooked expected = %v, got %v", tt.cooked, cooked)
+		}
 	}
-	record := cur.GetMetric("speed").GetRecords()[0]
-	if record != tt.record {
-		t.Errorf("record expected = %t, got %t", tt.record, record)
+
+	record := cur.GetMetric("speed").GetRecords()
+	for i := range record {
+		if record[i] != tt.record[i] {
+			t.Errorf("record expected = %t, got %t", tt.record, record)
+		}
 	}
 }
 
