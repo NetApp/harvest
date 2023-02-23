97249732404c21c41cc3b1b9ecb3b851cb8b3cd9
hardikl <hardikl@netapp.com>
2023-02-23 15:35:35 +0530

    fix: handled review comments

--- cmd/tools/grafana/dashboard_test.go
+++ cmd/tools/grafana/dashboard_test.go
@@ -228,7 +228,8 @@ func doPanel(t *testing.T, pathPrefix string, key gjson.Result, value gjson.Resu
 
 	// In case of gradient-gauge and percent(0.0-1.0), we must override min and max value
 	for _, properties := range propertiesMap {
-		if properties["unit"] == "percentunit" && properties["custom.displayMode"] == "gradient-gauge" {
+		displayMode := properties["custom.displayMode"]
+		if properties["unit"] == "percentunit" && (displayMode == "gradient-gauge" || displayMode == "lcd-gauge" || displayMode == "basic") {
 			if maxVal, exist := properties["max"]; !exist || maxVal != "1" {
 				t.Errorf("dashboard=%s, title=%s should have max value 1", sPath, title)
 			}
