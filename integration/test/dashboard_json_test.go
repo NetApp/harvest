fb23d48e96ea6cfe833f8beebea8a38704c91941
hardikl <hardikl@netapp.com>
2023-03-30 16:51:21 +0530

    feat: CI changes

--- integration/test/dashboard_json_test.go
+++ integration/test/dashboard_json_test.go
@@ -39,6 +39,7 @@ var zapiCounterMap = map[string]struct{}{
 	"ontaps3_logical_used_size": {},
 	"ontaps3_size":              {},
 	"ontaps3_object_count":      {},
+	"ontaps3_used_percent":      {},
 }
 
 // restCounters  consists of counters which needs to be excluded for Rest only in addition to counters present in counterMap
