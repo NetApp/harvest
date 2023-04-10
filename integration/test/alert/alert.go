6bf88b6764425e008878781282007d1c132c881d
hardikl <hardikl@netapp.com>
2023-04-10 17:03:55 +0530

    ci: handle comments and cluster scoped testing

--- integration/test/alert/alert.go
+++ integration/test/alert/alert.go
@@ -11,6 +11,9 @@ import (
 )
 
 const PrometheusAlertURL string = "http://localhost:9090/api/v1/alerts"
+const TestClusterName = "umeng-aff300-05-06"
+const TestNodeName = "umeng-aff300-06"
+const User = "admin"
 
 var volumeArwState = []string{
 	`"disable-in-progress"`,
@@ -29,10 +32,6 @@ type PromAlert struct {
 	message string
 	count   int
 }
-type EmsData struct {
-	EmsName      string
-	resolvingEms string
-}
 
 func GetAlerts() (map[string]int, int) {
 	alertsData := make(map[string]int)
@@ -50,16 +49,21 @@ func GetAlerts() (map[string]int, int) {
 		for _, alert := range alerts.Array() {
 			labels := alert.Get("labels").Map()
 			alertData := labels["message"].String()
-			alertsData[alertData] = alertsData[alertData] + 1
-			totalAlerts++
+			clusterName := labels["cluster"].String()
+			if clusterName == TestClusterName {
+				alertsData[alertData] = alertsData[alertData] + 1
+				totalAlerts++
+			}
 		}
 	}
 	return alertsData, totalAlerts
 }
 
-func GetEmsAlerts(dir string, fileName string) ([]EmsData, []EmsData) {
-	totalEms := make([]EmsData, 0)
-	bookendEms := make([]EmsData, 0)
+func GetEmsAlerts(dir string, fileName string) ([]string, []string, []string) {
+	nonBookendEms := make([]string, 0)
+	issuingEms := make([]string, 0)
+	resolvingEms := make([]string, 0)
+	nonBookendEmsCount := 0
 	bookendEmsCount := 0
 
 	emsConfigFilePath := dir + "/" + fileName
@@ -73,17 +77,20 @@ func GetEmsAlerts(dir string, fileName string) ([]EmsData, []EmsData) {
 	for _, child := range data.GetChildS("events").GetChildren() {
 		emsName := child.GetChildContentS("name")
 		if resolveEms := child.GetChildS("resolve_when_ems"); resolveEms != nil {
-			bookendEms = append(bookendEms, EmsData{EmsName: emsName, resolvingEms: resolveEms.GetChildContentS("name")})
+			issuingEms = append(issuingEms, emsName)
+			resolvingEms = append(resolvingEms, resolveEms.GetChildContentS("name"))
 			bookendEmsCount++
+		} else {
+			nonBookendEms = append(nonBookendEms, emsName)
+			nonBookendEmsCount++
 		}
-		totalEms = append(totalEms, EmsData{EmsName: emsName})
 	}
 
-	log.Debug().Msgf("Total ems configured: %d, Bookend ems configured:%d", len(totalEms), bookendEmsCount)
-	return totalEms, bookendEms
+	log.Info().Msgf("Total ems configured: %d, Non-Bookend ems configured:%d, Bookend ems configured:%d", nonBookendEmsCount+bookendEmsCount, nonBookendEmsCount, bookendEmsCount)
+	return nonBookendEms, issuingEms, resolvingEms
 }
 
-func GenerateEvents(emsNames []EmsData, nodeScopedEms []string) []string {
+func GenerateEvents(emsNames []string, nodeScopedEms []string) []string {
 	supportedEms := make([]string, 0)
 	var jsonValue []byte
 	addr, user, pass, nodeName := GetPollerDetail()
@@ -92,12 +99,8 @@ func GenerateEvents(emsNames []EmsData, nodeScopedEms []string) []string {
 
 	volumeArwCount := 0
 	vserverArwCount := 0
-	for _, e := range emsNames {
+	for _, ems := range emsNames {
 		value := "1"
-		ems := ""
-		if ems = e.resolvingEms; ems == "" {
-			ems = e.EmsName
-		}
 		if ems == "arw.volume.state" {
 			value = volumeArwState[volumeArwCount]
 			volumeArwCount++
@@ -113,6 +116,7 @@ func GenerateEvents(emsNames []EmsData, nodeScopedEms []string) []string {
 		} else {
 			jsonValue = []byte(fmt.Sprintf(`{"message-name": "%s", "values": [%s,2,3,4,5,6,7,8,9]}`, ems, value))
 		}
+
 		var data map[string]interface{}
 		data = utils.SendPostReqAndGetRes(url, method, jsonValue, user, pass)
 		if response := data["error"]; response != nil {
@@ -120,10 +124,10 @@ func GenerateEvents(emsNames []EmsData, nodeScopedEms []string) []string {
 			code := errorDetail["code"].(string)
 			target := errorDetail["target"].(string)
 			if !(code == "2" && target == "message-name") {
-				supportedEms = append(supportedEms, e.EmsName)
+				supportedEms = append(supportedEms, ems)
 			}
 		} else {
-			supportedEms = append(supportedEms, e.EmsName)
+			supportedEms = append(supportedEms, ems)
 		}
 	}
 
@@ -140,9 +144,9 @@ func GetPollerDetail() (string, string, string, string) {
 		utils.PanicIfNotNil(err)
 	}
 
-	if poller, err = conf.PollerNamed("umeng_aff300"); err != nil {
+	if poller, err = conf.PollerNamed(TestClusterName); err != nil {
 		utils.PanicIfNotNil(err)
 	}
 
-	return poller.Addr, "admin", poller.Password, "umeng-aff300-06"
+	return poller.Addr, User, poller.Password, TestNodeName
 }
