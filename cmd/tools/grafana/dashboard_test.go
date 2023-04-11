4aeade50dc8ae9947cd181bc0c7f19afc3c598cd
Rahul Gupta <rahul.gupta@netapp.com>
2023-04-11 22:12:48 +0530

    fix: restrict exemplar flag in dashboards

--- cmd/tools/grafana/dashboard_test.go
+++ cmd/tools/grafana/dashboard_test.go
@@ -487,6 +487,24 @@ func TestIDIsBlank(t *testing.T) {
 		})
 }
 
+func TestExemplarTrue(t *testing.T) {
+	visitDashboards(
+		[]string{"../../../grafana/dashboards/cmode", "../../../grafana/dashboards/storagegrid"},
+		func(path string, data []byte) {
+			checkExemplarTrue(t, path, data)
+		})
+}
+
+func checkExemplarTrue(t *testing.T, path string, data []byte) {
+
+	gjson.ForEachLine(string(data), func(line gjson.Result) bool {
+		if strings.Contains(line.String(), "\"exemplar\": true") {
+			t.Errorf(`dashboard=%s exemplar should be "false" but is "true"`, shortPath(path))
+		}
+		return true
+	})
+}
+
 func checkUIDIsBlank(t *testing.T, path string, data []byte) {
 	uid := gjson.GetBytes(data, "uid").String()
 	if uid != "" {
