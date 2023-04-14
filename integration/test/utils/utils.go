6dc9cc6b6e16f275ba7a5dee063599d1994cad9f
Chris Grindstaff <chris@gstaff.org>
2023-04-14 11:13:06 -0400

    ci: switch cert to harvest_cert.yml (#1937)

--- integration/test/utils/utils.go
+++ integration/test/utils/utils.go
@@ -136,8 +136,7 @@ func RemoveDir(dir string) error {
 }
 
 func UseCertFile(harvestHome string) {
-	// Copy harvest_cert_2023.yml from /u/ to local
-	harvestCertFile := "harvest_cert_2023.yml"
+	harvestCertFile := "harvest_cert.yml"
 	harvestFile := "harvest.yml"
 	Run("cp", "-p", GetConfigDir()+"/"+harvestCertFile, harvestHome+"/"+harvestFile)
 	Run("certer", "-ip", "10.193.48.11")
