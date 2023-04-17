d09ade49cc05297955733f21d305ffeb9c70130c
Chris Grindstaff <chris@gstaff.org>
2023-04-17 09:34:48 -0400

    ci: should fail when logs contain errors

--- integration/test/copy_logs_test.go
+++ integration/test/copy_logs_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/Netapp/harvest-automation/test/docker"
 	"github.com/Netapp/harvest-automation/test/installer"
 	"github.com/Netapp/harvest-automation/test/utils"
+	"os/exec"
 	"testing"
 )
 
@@ -17,5 +20,40 @@ func TestCopyLogs(t *testing.T) {
 	for _, containerId := range containerIds {
 		containerShortId := containerId[:10]
 		docker.StoreContainerLog(containerShortId, harvestLogDir+"/"+containerShortId+".log")
+	}
+}
+
+func TestNoErrors(t *testing.T) {
+	utils.SkipIfMissing(t, utils.AnalyzeDockerLogs)
+	containerIds := docker.GetContainerID("bin/poller")
+	for _, containerId := range containerIds {
+		checkLogs(t, containerId)
+	}
+}
+
+func checkLogs(t *testing.T, container string) {
+	cli := fmt.Sprintf(`docker logs %s 2>&1 | grep -E "ERR"`, container)
+	command := exec.Command("bash", "-c", cli)
+	output, err := command.CombinedOutput()
+	// The grep checks for matching lines.
+	// An exit status of:
+	//   0 means one or more lines matched
+	//   1 means no lines matched
+	//  >1 means an error occurred
+	if err != nil {
+		var ee *exec.ExitError
+		if errors.As(err, &ee) {
+			// an exit code of 1 means there were no matches, that's OK
+			if ee.ExitCode() == 1 {
+				return
+
+			}
+		}
+		t.Errorf("ERR checking logs container=%s cli=%s err=%v output=%s", container, cli, err, string(output))
+		return
+	}
+	if len(output) > 0 {
+		t.Errorf("ERRs found in poller logs container=%s size=%d. Dump of errors follows:\n%s",
+			container, len(output), string(output))
 	}
 }
