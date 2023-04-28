ded28a6a7b85f5f63961d50e77eabb0c32ccdafd
Rahul Gupta <rahul.gupta@netapp.com>
2023-04-28 16:24:46 +0530

    ci: fix ci ems test case

--- integration/test/alert/alert.go
+++ integration/test/alert/alert.go
@@ -39,7 +39,7 @@ func GetAlerts() (map[string]int, int) {
 	alertsData := make(map[string]int)
 	totalAlerts := 0
 
-	duration, _ := time.ParseDuration("3m15s")
+	duration, _ := time.ParseDuration("3m30s")
 	time.Sleep(duration)
 	response, err := utils.GetResponseBody(PrometheusAlertURL)
 	utils.PanicIfNotNil(err)
