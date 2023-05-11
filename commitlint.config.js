85506d8d0898f4dc3d8bf0c492952c91c91f223f
Chris Grindstaff <chris@gstaff.org>
2023-05-11 18:05:20 -0400

    ci: renovate should ignore integration

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,6 +11,7 @@ module.exports = {
             [
                 'collector',
                 'config',
+                'deps',
                 'exporter',
                 'grafana',
                 'influxdb',
