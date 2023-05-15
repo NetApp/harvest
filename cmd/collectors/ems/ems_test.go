6eebce22b96b9726740aab4c285ea763cfc73013
hardikl <hardikl@netapp.com>
2023-05-15 15:49:56 +0530

    test: handle review comments

--- cmd/collectors/ems/ems_test.go
+++ cmd/collectors/ems/ems_test.go
@@ -23,12 +23,101 @@ func Test_Ems(t *testing.T) {
 
 // Bookend ems test-case would also handle the workflow of non-bookend ems test-case.
 func BookendEmsTest(t *testing.T, e *Ems) {
+	// Testcase: Auto-resolved bookend ems after time expires. Generate ems now and at last evaluate the cache for existence of these ems.
+	e.updateMatrix()
+	// Simulated bookend issuing ems "LUN.offline" and ems "monitor.fan.critical", they will be in cache until their resolve after time expires
+	autoresolveEmsNames := []string{"LUN.offline", "monitor.fan.critical"}
+	results := collectors.JSONToGson("testdata/autoresolveEms.json", true)
+	// Polling ems collector to handle results
+	if _, emsCount := e.HandleResults(results, e.emsProp); emsCount == 0 {
+		t.Fatal("Failed to fetch data")
+	}
+	// Check and evaluate bookend ems events got generated successfully, e.Matrix map would have one entry of Ems(parent) in map.
+	if len(e.Matrix) != len(autoresolveEmsNames)+1 {
+		t.Error("Not all bookend ems event have been generated")
+	}
+	for generatedEmsName := range e.Matrix {
+		if !util.Contains(autoresolveEmsNames, generatedEmsName) && generatedEmsName != "Ems" {
+			t.Errorf("Extra ems event has been detected= %s", generatedEmsName)
+		}
+	}
+
+	// Bookend EMS testing: Simulated bookend issuing ems "wafl.vvol.offline" and ems "hm.alert.raised" with alert_id value as "RaidLeftBehindAggrAlert"
+	issuingEmsNames := []string{"wafl.vvol.offline", "hm.alert.raised"}
 	// Step 1: Generate Bookend issuing ems
+	e.testBookendIssuingEms(t, issuingEmsNames, "testdata/issuingEms.json")
+
+	// Step 2: Generate Bookend resolving ems
+	e.testBookendResolvingEms(t, issuingEmsNames, "testdata/resolvingEms.json")
+
+	// Evaluate the cache for existence of these auto resolve ems.
+	// Sleep for 1 second and check LUN.offline ems got auto resolved
+	time.Sleep(1 * time.Second)
+	e.updateMatrix()
+	// Check and evaluate bookend ems events got auto resolved successfully, e.Matrix map would have one entry of Ems(parent) in map.
+	if len(e.Matrix) != 2 {
+		t.Error("Bookend ems event haven't been auto resolved")
+	}
+	for generatedEmsName := range e.Matrix {
+		if generatedEmsName != "monitor.fan.critical" && generatedEmsName != "Ems" {
+			t.Errorf("This bookend ems event haven't been auto resolved= %s", generatedEmsName)
+		}
+	}
+
+	// Sleep for another 1 second and check both the ems got auto resolved
+	time.Sleep(1 * time.Second)
+	e.updateMatrix()
+	// Check and evaluate bookend ems events got auto resolved successfully, e.Matrix map would have one entry of Ems(parent) in map.
+	if len(e.Matrix) != 1 {
+		t.Error("Bookend ems event haven't been auto resolved")
+	}
+	for generatedEmsName := range e.Matrix {
+		if generatedEmsName != "Ems" {
+			t.Errorf("This bookend ems event haven't been auto resolved= %s", generatedEmsName)
+		}
+	}
+}
+
+func NewEms() *Ems {
+	// homepath is harvest directory level
+	homePath := "../../../"
+	emsConfgPath := homePath + "conf/ems/default.yaml"
+	emsPoller := "testEms"
+
+	conf.TestLoadHarvestConfig("testdata/config.yml")
+	opts := options.Options{
+		Poller:   emsPoller,
+		HomePath: homePath,
+		IsTest:   true,
+	}
+	ac := collector.New("Ems", "Ems", &opts, emsParams(emsConfgPath), nil)
+	e := &Ems{}
+	if err := e.Init(ac); err != nil {
+		log.Fatal().Err(err)
+	}
+	// Changed the resolve_after for 2 issuing ems for auto resolve testing
+	e.resolveAfter["LUN.offline"] = 1 * time.Second
+	e.resolveAfter["monitor.fan.critical"] = 2 * time.Second
+	return e
+}
+
+func emsParams(emsConfigPath string) *node.Node {
+	bytes, err := os.ReadFile(emsConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	root, err := tree.LoadYaml(bytes)
+	if err != nil {
+		panic(err)
+	}
+	return root
+}
+
+func (e *Ems) testBookendIssuingEms(t *testing.T, issuingEmsNames []string, path string) {
 	e.updateMatrix()
 
-	// Simulated bookend issuing ems "wafl.vvol.offline" and ems "hm.alert.raised" with alert_id value as "RaidLeftBehindAggrAlert"
-	issuingEmsNames := []string{"wafl.vvol.offline", "hm.alert.raised"}
-	results := collectors.JSONToGson("testdata/ems-poll-1.json", true)
+	results := collectors.JSONToGson(path, true)
 	// Polling ems collector to handle results
 	if _, emsCount := e.HandleResults(results, e.emsProp); emsCount == 0 {
 		t.Fatal("Failed to fetch data")
@@ -57,19 +146,18 @@ func BookendEmsTest(t *testing.T, e *Ems) {
 					}
 				}
 			}
-		} else if generatedEmsName != "Ems" {
-			t.Errorf("Extra bookend issuing ems event found= %s", generatedEmsName)
 		}
 	}
 	if len(notGeneratedEmsNames) > 0 {
 		t.Fatalf("These Bookend Ems haven't been raised: %s", notGeneratedEmsNames)
 	}
+}
 
-	// Step 2: Generate Bookend resolving ems
+func (e *Ems) testBookendResolvingEms(t *testing.T, issuingEmsNames []string, path string) {
 	e.updateMatrix()
 
 	// Simulated bookend resolving ems "wafl.vvol.online" and ems "hm.alert.cleared" with alert_id value as "RaidLeftBehindAggrAlert"
-	results = collectors.JSONToGson("testdata/ems-poll-2.json", true)
+	results := collectors.JSONToGson(path, true)
 	// Polling ems collector to handle results
 	if _, emsCount := e.HandleResults(results, e.emsProp); emsCount == 0 {
 		t.Fatal("Failed to fetch data")
@@ -99,95 +187,10 @@ func BookendEmsTest(t *testing.T, e *Ems) {
 					}
 				}
 			}
-		} else if generatedEmsName != "Ems" {
-			t.Errorf("Extra bookend issuing ems event found= %s", generatedEmsName)
 		}
 	}
 	// After resolving ems, all bookend ems should resolved
 	if len(notResolvedEmsNames) > 0 {
 		t.Errorf("These Bookend Ems haven't been resolved: %s", notResolvedEmsNames)
 	}
-
-	// Step 3: Auto-resolved bookend ems after time expired
-	e.updateMatrix()
-
-	// Simulated bookend issuing ems "LUN.offline" and ems "monitor.fan.critical"
-	issuingEmsNames = []string{"LUN.offline", "monitor.fan.critical"}
-	results = collectors.JSONToGson("testdata/ems-poll-3.json", true)
-	// Polling ems collector to handle results
-	if _, emsCount := e.HandleResults(results, e.emsProp); emsCount == 0 {
-		t.Fatal("Failed to fetch data")
-	}
-
-	// Check and evaluate bookend ems events got generated successfully, e.Matrix map would have one entry of Ems(parent) in map.
-	if len(e.Matrix) != len(issuingEmsNames)+1 {
-		t.Error("Not all bookend ems event have been generated")
-	}
-	for generatedEmsName := range e.Matrix {
-		if !util.Contains(issuingEmsNames, generatedEmsName) && generatedEmsName != "Ems" {
-			t.Errorf("Extra ems event has been detected= %s", generatedEmsName)
-		}
-	}
-	// Changed the resolve_after for 2 issuing ems
-	e.resolveAfter["LUN.offline"] = 1 * time.Minute
-	e.resolveAfter["monitor.fan.critical"] = 2 * time.Minute
-
-	// Sleep for 1 minute and check LUN.offline ems got auto resolved
-	time.Sleep(1 * time.Minute)
-	e.updateMatrix()
-	// Check and evaluate bookend ems events got generated successfully, e.Matrix map would have one entry of Ems(parent) in map.
-	if len(e.Matrix) != 2 {
-		t.Error("Bookend ems event haven't been auto resolved")
-	}
-	for generatedEmsName := range e.Matrix {
-		if generatedEmsName != "monitor.fan.critical" && generatedEmsName != "Ems" {
-			t.Errorf("This bookend ems event haven't been auto resolved= %s", generatedEmsName)
-		}
-	}
-
-	// Sleep for another 1 minute and check both the ems got auto resolved
-	time.Sleep(1 * time.Minute)
-	e.updateMatrix()
-	// Check and evaluate bookend ems events got generated successfully, e.Matrix map would have one entry of Ems(parent) in map.
-	if len(e.Matrix) != 1 {
-		t.Error("Bookend ems event haven't been auto resolved")
-	}
-	for generatedEmsName := range e.Matrix {
-		if generatedEmsName != "Ems" {
-			t.Errorf("This bookend ems event haven't been auto resolved= %s", generatedEmsName)
-		}
-	}
-}
-
-func NewEms() *Ems {
-	// homepath is harvest directory level
-	homePath := "../../../"
-	emsConfgPath := homePath + "conf/ems/default.yaml"
-	emsPoller := "testEms"
-
-	conf.TestLoadHarvestConfig("testdata/config.yml")
-	opts := options.Options{
-		Poller:   emsPoller,
-		HomePath: homePath,
-		IsTest:   true,
-	}
-	ac := collector.New("Ems", "Ems", &opts, emsParams(emsConfgPath), nil)
-	e := &Ems{}
-	if err := e.Init(ac); err != nil {
-		log.Fatal().Err(err)
-	}
-	return e
-}
-
-func emsParams(emsConfigPath string) *node.Node {
-	bytes, err := os.ReadFile(emsConfigPath)
-	if err != nil {
-		panic(err)
-	}
-
-	root, err := tree.LoadYaml(bytes)
-	if err != nil {
-		panic(err)
-	}
-	return root
 }
