f360dfd7571698c95aaf8b5de304ba066e4a99a7
Rahul <rahul.gupta@netapp.com>
2023-05-29 17:36:10 +0530

    feat: workload policy iops metrics (#2111)
    * feat: workload policy iops metrics
    
    * feat: workload policy iops metrics

--- cmd/collectors/rest/plugins/qospolicyfixed/qospolicyfixed.go
+++ cmd/collectors/rest/plugins/qospolicyfixed/qospolicyfixed.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 )
 
+var metrics = []string{
+	"max_throughput_iops",
+	"max_throughput_mbps",
+	"min_throughput_iops",
+	"min_throughput_mbps",
+}
+
 type QosPolicyFixed struct {
 	*plugin.AbstractPlugin
 }
@@ -18,14 +25,23 @@ func New(p *plugin.AbstractPlugin) plugin.Plugin {
 func (p *QosPolicyFixed) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, error) {
 	data := dataMap[p.Object]
 
+	// create metrics
+	for _, k := range metrics {
+		err := matrix.CreateMetric(k, data)
+		if err != nil {
+			p.Logger.Error().Err(err).Str("key", k).Msg("error while creating metric")
+			return nil, err
+		}
+	}
+
 	for _, instance := range data.GetInstances() {
-		p.setFixed(instance)
+		p.setFixed(data, instance)
 	}
 
 	return nil, nil
 }
 
-func (p *QosPolicyFixed) setFixed(instance *matrix.Instance) {
+func (p *QosPolicyFixed) setFixed(data *matrix.Matrix, instance *matrix.Instance) {
 	label := instance.GetLabel("throughput_policy")
 	if label == "" {
 		return
@@ -45,8 +61,19 @@ func (p *QosPolicyFixed) setFixed(instance *matrix.Instance) {
 		p.Logger.Error().Err(err).Str("label", after).Msg("Failed to parse fixed xput label")
 		return
 	}
-	instance.SetLabel("min_throughput_iops", min.IOPS)
-	instance.SetLabel("max_throughput_iops", max.IOPS)
-	instance.SetLabel("min_throughput_mbps", min.Mbps)
-	instance.SetLabel("max_throughput_mbps", max.Mbps)
+	p.setLabel("min_throughput_iops", data, instance, min.IOPS)
+	p.setLabel("max_throughput_iops", data, instance, max.IOPS)
+	p.setLabel("min_throughput_mbps", data, instance, min.Mbps)
+	p.setLabel("max_throughput_mbps", data, instance, max.Mbps)
+}
+
+func (p *QosPolicyFixed) setLabel(labelName string, data *matrix.Matrix, instance *matrix.Instance, value string) {
+	instance.SetLabel(labelName, value)
+	m := data.GetMetric(labelName)
+	if m != nil {
+		err := m.SetValueString(instance, value)
+		if err != nil {
+			p.Logger.Error().Str(labelName, value).Err(err).Msg("Unable to set metric")
+		}
+	}
 }
