07fbf34fe6782e1eb9aad56a1f466ca7c6de24d3
hardikl <hardikl@netapp.com>
2024-03-06 14:17:17 +0530

    feat: handled review comments

--- cmd/collectors/zapi/plugins/systemnode/systemnode.go
+++ cmd/collectors/zapi/plugins/systemnode/systemnode.go
@@ -13,65 +13,67 @@ import (
 
 type SystemNode struct {
 	*plugin.AbstractPlugin
-	client         *zapi.Client
-	partnerNameMap map[string]string // node-name -> partner name map
+	client *zapi.Client
 }
 
 func New(p *plugin.AbstractPlugin) plugin.Plugin {
 	return &SystemNode{AbstractPlugin: p}
 }
 
-func (a *SystemNode) Init() error {
+func (s *SystemNode) Init() error {
 
 	var err error
 
-	if err = a.InitAbc(); err != nil {
+	if err = s.InitAbc(); err != nil {
 		return err
 	}
 
-	if a.client, err = zapi.New(conf.ZapiPoller(a.ParentParams), a.Auth); err != nil {
-		a.Logger.Error().Stack().Err(err).Msg("connecting")
+	if s.client, err = zapi.New(conf.ZapiPoller(s.ParentParams), s.Auth); err != nil {
+		s.Logger.Error().Stack().Err(err).Msg("connecting")
 		return err
 	}
 
-	if err = a.client.Init(5); err != nil {
+	if err = s.client.Init(5); err != nil {
 		return err
 	}
 
-	a.partnerNameMap = make(map[string]string)
 	return nil
 }
 
-func (a *SystemNode) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, error) {
-	data := dataMap[a.Object]
+func (s *SystemNode) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, error) {
+	data := dataMap[s.Object]
 
 	// invoke system-get-node-info-iter zapi and populate node info
-	if err := a.getPartnerNodeInfo(); err != nil {
+	partnerNameMap, err := s.getPartnerNodeInfo()
+	if err != nil {
 		if errors.Is(err, errs.ErrNoInstance) {
-			a.Logger.Debug().Err(err).Msg("Failed to collect cloud store data")
+			s.Logger.Debug().Err(err).Msg("Failed to collect partner node detail")
+		} else {
+			s.Logger.Error().Err(err).Msg("Failed to collect partner node detail")
 		}
 	}
 
 	// update node instance with partner info
 	for nodeName, node := range data.GetInstances() {
-		node.SetLabel("ha_partner", a.partnerNameMap[nodeName])
+		node.SetLabel("ha_partner", partnerNameMap[nodeName])
 	}
 
 	// update node metrics with partner info
 	for _, metric := range data.GetMetrics() {
-		metric.SetLabel("ha_partner", a.partnerNameMap[metric.GetLabel("node")])
+		metric.SetLabel("ha_partner", partnerNameMap[metric.GetLabel("node")])
 	}
 	return nil, nil
 }
 
-func (a *SystemNode) getPartnerNodeInfo() error {
+func (s *SystemNode) getPartnerNodeInfo() (map[string]string, error) {
 	var (
-		result []*node.Node
-		err    error
+		result         []*node.Node
+		partnerNameMap map[string]string // node-name -> partner name map
+		err            error
 	)
 
 	// system-get-node-info-iter zapi
-	a.partnerNameMap = make(map[string]string)
+	partnerNameMap = make(map[string]string)
 	request := node.NewXMLS("system-get-node-info-iter")
 	request.NewChildS("max-records", collectors.DefaultBatchSize)
 
@@ -82,18 +84,18 @@ func (a *SystemNode) getPartnerNodeInfo() error {
 	desired.AddChild(systemInfo)
 	request.AddChild(desired)
 
-	if result, err = a.client.InvokeZapiCall(request); err != nil {
-		return err
+	if result, err = s.client.InvokeZapiCall(request); err != nil {
+		return nil, err
 	}
 
 	if len(result) == 0 || result == nil {
-		return errs.New(errs.ErrNoInstance, "no records found")
+		return nil, errs.New(errs.ErrNoInstance, "no records found")
 	}
 
 	for _, objectStore := range result {
 		partnerName := objectStore.GetChildContentS("partner-system-name")
 		nodeName := objectStore.GetChildContentS("system-name")
-		a.partnerNameMap[nodeName] = partnerName
+		partnerNameMap[nodeName] = partnerName
 	}
-	return nil
+	return partnerNameMap, nil
 }
