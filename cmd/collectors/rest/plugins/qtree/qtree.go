e7b1755cd6417e780059ff21218c095728ee5016
Hardikl <83282894+Hardikl@users.noreply.github.com>
2024-05-09 11:34:29 +0530

    fix: Handling index for quota (#2874)
    * fix: Handling index for quota
    
    * feat: Removing index for quota and handle in grafana panels
    
    * fix: changed filter query

--- cmd/collectors/rest/plugins/qtree/qtree.go
+++ cmd/collectors/rest/plugins/qtree/qtree.go
@@ -177,7 +177,7 @@ func (q *Qtree) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 		err = q.handlingHistoricalMetrics(result, data, cluster, &quotaCount, &numMetrics)
 	} else {
 		// Populate metrics with quota prefix and current labels
-		err = q.handlingQuotaMetrics(result, cluster, &quotaCount, &numMetrics)
+		err = q.handlingQuotaMetrics(result, &quotaCount, &numMetrics)
 	}
 
 	if err != nil {
@@ -289,8 +289,8 @@ func (q *Qtree) handlingHistoricalMetrics(result []gjson.Result, data *matrix.Ma
 	return nil
 }
 
-func (q *Qtree) handlingQuotaMetrics(result []gjson.Result, cluster string, quotaCount *int, numMetrics *int) error {
-	for quotaIndex, quota := range result {
+func (q *Qtree) handlingQuotaMetrics(result []gjson.Result, quotaCount *int, numMetrics *int) error {
+	for _, quota := range result {
 		var tree string
 
 		if !quota.IsObject() {
@@ -313,7 +313,7 @@ func (q *Qtree) handlingQuotaMetrics(result []gjson.Result, cluster string, quot
 			// set -1 for unlimited
 			value := -1.0
 
-			quotaInstanceKey := strconv.Itoa(quotaIndex) + "." + attribute
+			quotaInstanceKey := vserver + "." + volume + "." + tree + "." + group + "." + uName + "." + attribute
 
 			quotaInstance, err := q.data.NewInstance(quotaInstanceKey)
 			if err != nil {
@@ -325,7 +325,6 @@ func (q *Qtree) handlingQuotaMetrics(result []gjson.Result, cluster string, quot
 			quotaInstance.SetLabel("qtree", tree)
 			quotaInstance.SetLabel("volume", volume)
 			quotaInstance.SetLabel("svm", vserver)
-			quotaInstance.SetLabel("index", cluster+"_"+strconv.Itoa(quotaIndex))
 
 			if quotaType == "user" {
 				if uName != "" {
--- cmd/collectors/zapi/plugins/qtree/qtree.go
+++ cmd/collectors/zapi/plugins/qtree/qtree.go
@@ -208,7 +208,7 @@ func (q *Qtree) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 			err = q.handlingHistoricalMetrics(quotas, data, cluster, &quotaIndex, &numMetrics)
 		} else {
 			// Populate metrics with quota prefix and current labels
-			err = q.handlingQuotaMetrics(quotas, cluster, &quotaIndex, &numMetrics)
+			err = q.handlingQuotaMetrics(quotas, &quotaIndex, &numMetrics)
 		}
 
 		if err != nil {
@@ -322,7 +322,7 @@ func (q *Qtree) handlingHistoricalMetrics(quotas []*node.Node, data *matrix.Matr
 	return nil
 }
 
-func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, cluster string, quotaIndex *int, numMetrics *int) error {
+func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, quotaCount *int, numMetrics *int) error {
 	for _, quota := range quotas {
 		var vserver, quotaInstanceKey, uid, uName string
 
@@ -358,7 +358,7 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, cluster string, quotaI
 				continue
 			}
 		}
-		*quotaIndex++
+		*quotaCount++
 
 		for attribute, m := range q.data.GetMetrics() {
 
@@ -371,9 +371,9 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, cluster string, quotaI
 
 				// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
 				if q.client.IsClustered() {
-					quotaInstanceKey = vserver + "." + volume + "." + tree + "." + strconv.Itoa(*quotaIndex) + "." + attribute
+					quotaInstanceKey = vserver + "." + volume + "." + tree + "." + uName + "." + attribute
 				} else {
-					quotaInstanceKey = volume + "." + tree + "." + strconv.Itoa(*quotaIndex) + "." + attribute
+					quotaInstanceKey = volume + "." + tree + "." + uName + "." + attribute
 				}
 				quotaInstance, err := q.data.NewInstance(quotaInstanceKey)
 				if err != nil {
@@ -385,7 +385,6 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, cluster string, quotaI
 				quotaInstance.SetLabel("qtree", tree)
 				quotaInstance.SetLabel("volume", volume)
 				quotaInstance.SetLabel("svm", vserver)
-				quotaInstance.SetLabel("index", cluster+"_"+strconv.Itoa(*quotaIndex))
 
 				if quotaType == "user" {
 					if uName != "" {
