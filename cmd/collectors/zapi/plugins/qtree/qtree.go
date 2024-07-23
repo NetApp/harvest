c4dc19d9b4294955fa49aeb320caff53dc0ce73d
Hardikl <83282894+Hardikl@users.noreply.github.com>
2024-07-23 22:16:32 +0530

    feat: handled user/group quota in HistoricalLabels (#3060)
    * feat: handled user/group quota in HistoricalLabels for zapi/rest

--- cmd/collectors/zapi/plugins/qtree/qtree.go
+++ cmd/collectors/zapi/plugins/qtree/qtree.go
@@ -89,8 +89,9 @@ func (q *Qtree) Init() error {
 		}
 
 		instanceKeys.NewChildS("", "type")
-		instanceKeys.NewChildS("", "index")
 		instanceKeys.NewChildS("", "unit")
+		instanceKeys.NewChildS("", "user")
+		instanceKeys.NewChildS("", "group")
 
 		q.data.SetExportOptions(exportOptions)
 		q.historicalLabels = true
@@ -177,8 +178,6 @@ func (q *Qtree) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 		}
 	}
 
-	cluster := data.GetGlobalLabels()["cluster"]
-
 	for {
 		response, tag, ad, pd, err = q.client.InvokeBatchWithTimers(request, tag)
 
@@ -208,13 +207,8 @@ func (q *Qtree) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 
 		q.Logger.Debug().Int("quotas", len(quotas)).Msg("fetching quotas")
 
-		if q.historicalLabels {
-			// In 22.05, populate metrics with qtree prefix and old labels
-			err = q.handlingHistoricalMetrics(quotas, data, cluster, &quotaIndex, &numMetrics)
-		} else {
-			// Populate metrics with quota prefix and current labels
-			err = q.handlingQuotaMetrics(quotas, &quotaIndex, &numMetrics)
-		}
+		// Populate metrics with quota prefix
+		err = q.handlingQuotaMetrics(quotas, data, &quotaIndex, &numMetrics)
 
 		if err != nil {
 			return nil, nil, err
@@ -242,99 +236,10 @@ func (q *Qtree) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 	return []*matrix.Matrix{q.data}, q.client.Metadata, nil
 }
 
-func (q *Qtree) handlingHistoricalMetrics(quotas []*node.Node, data *matrix.Matrix, cluster string, quotaIndex *int, numMetrics *int) error {
-	for qIndex, quota := range quotas {
-		var vserver, quotaInstanceKey string
-		var qtreeInstance *matrix.Instance
-
-		quotaType := quota.GetChildContentS("quota-type")
-		tree := quota.GetChildContentS("tree")
-		volume := quota.GetChildContentS("volume")
-		if q.client.IsClustered() {
-			vserver = quota.GetChildContentS("vserver")
-		}
-
-		*quotaIndex++
-		for attribute, m := range q.data.GetMetrics() {
-
-			objectElem := quota.GetChildS(attribute)
-			if objectElem == nil {
-				continue
-			}
-
-			if attrValue := quota.GetChildContentS(attribute); attrValue != "" {
-				if q.client.IsClustered() {
-					qtreeInstance = data.GetInstance(tree + "." + volume + "." + vserver)
-				} else {
-					qtreeInstance = data.GetInstance(volume + "." + tree)
-				}
-				if qtreeInstance == nil {
-					q.Logger.Warn().
-						Str("tree", tree).
-						Str("volume", volume).
-						Str("vserver", vserver).
-						Msg("No instance matching tree.volume.vserver")
-					continue
-				}
-				if !qtreeInstance.IsExportable() {
-					continue
-				}
-				// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
-				if q.client.IsClustered() {
-					quotaInstanceKey = vserver + "." + volume + "." + tree + "." + strconv.Itoa(qIndex) + "." + attribute
-				} else {
-					quotaInstanceKey = volume + "." + tree + "." + strconv.Itoa(qIndex) + "." + attribute
-				}
-				quotaInstance, err := q.data.NewInstance(quotaInstanceKey)
-
-				if err != nil {
-					q.Logger.Debug().Msgf("add (%s) instance: %v", attribute, err)
-					return err
-				}
-
-				for _, label := range q.data.GetExportOptions().GetChildS("instance_keys").GetAllChildContentS() {
-					if value := qtreeInstance.GetLabel(label); value != "" {
-						quotaInstance.SetLabel(label, value)
-					}
-				}
-
-				// set labels
-				quotaInstance.SetLabel("type", quotaType)
-				quotaInstance.SetLabel("qtree", tree)
-				quotaInstance.SetLabel("volume", volume)
-				quotaInstance.SetLabel("svm", vserver)
-				quotaInstance.SetLabel("index", cluster+"_"+strconv.Itoa(*quotaIndex))
-
-				// If the Qtree is the volume itself, then qtree label is empty, so copy the volume name to qtree.
-				if tree == "" {
-					quotaInstance.SetLabel("qtree", volume)
-				}
-
-				// populate numeric data
-				if value := strings.Split(attrValue, " ")[0]; value != "" {
-					// Few quota metrics would have value '-' which means unlimited (ex: disk-limit)
-					if value == "-" {
-						value = "0"
-					}
-					if attribute == "soft-disk-limit" || attribute == "disk-limit" || attribute == "disk-used" {
-						quotaInstance.SetLabel("unit", "Kbyte")
-					}
-					if err := m.SetValueString(quotaInstance, value); err != nil {
-						q.Logger.Debug().Msgf("(%s) failed to parse value (%s): %v", attribute, value, err)
-					} else {
-						*numMetrics++
-					}
-				}
-
-			}
-		}
-	}
-	return nil
-}
-
-func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, quotaCount *int, numMetrics *int) error {
+func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, data *matrix.Matrix, quotaCount *int, numMetrics *int) error {
 	for _, quota := range quotas {
 		var vserver, quotaInstanceKey, uid, uName string
+		var qtreeInstance *matrix.Instance
 
 		quotaType := quota.GetChildContentS("quota-type")
 		tree := quota.GetChildContentS("tree")
@@ -370,15 +275,33 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, quotaCount *int, numMe
 		}
 		*quotaCount++
 
-		for attribute, m := range q.data.GetMetrics() {
+		// In 22.05, populate metrics with qtree labels
+		if q.historicalLabels {
+			if q.client.IsClustered() {
+				qtreeInstance = data.GetInstance(tree + "." + volume + "." + vserver)
+			} else {
+				qtreeInstance = data.GetInstance(volume + "." + tree)
+			}
+			if qtreeInstance == nil {
+				q.Logger.Warn().
+					Str("tree", tree).
+					Str("volume", volume).
+					Str("vserver", vserver).
+					Msg("No instance matching tree.volume.vserver")
+				continue
+			}
+			if !qtreeInstance.IsExportable() {
+				continue
+			}
+		}
 
+		for attribute, m := range q.data.GetMetrics() {
 			objectElem := quota.GetChildS(attribute)
 			if objectElem == nil {
 				continue
 			}
 
 			if attrValue := quota.GetChildContentS(attribute); attrValue != "" {
-
 				// Ex. InstanceKey: SVMA.vol1Abc.qtree1.5.disk-limit
 				if q.client.IsClustered() {
 					quotaInstanceKey = vserver + "." + volume + "." + tree + "." + uName + "." + attribute + "." + quotaType
@@ -390,11 +313,25 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, quotaCount *int, numMe
 					q.Logger.Debug().Msgf("add (%s) instance: %v", attribute, err)
 					return err
 				}
+
 				// set labels
 				quotaInstance.SetLabel("type", quotaType)
 				quotaInstance.SetLabel("qtree", tree)
 				quotaInstance.SetLabel("volume", volume)
 				quotaInstance.SetLabel("svm", vserver)
+
+				// In 22.05, populate metrics with qtree labels
+				if q.historicalLabels {
+					for _, label := range q.data.GetExportOptions().GetChildS("instance_keys").GetAllChildContentS() {
+						if value := qtreeInstance.GetLabel(label); value != "" {
+							quotaInstance.SetLabel(label, value)
+						}
+					}
+					// If the Qtree is the volume itself, then qtree label is empty, so copy the volume name to qtree.
+					if tree == "" {
+						quotaInstance.SetLabel("qtree", volume)
+					}
+				}
 
 				if quotaType == "user" {
 					if uName != "" {
@@ -414,7 +351,12 @@ func (q *Qtree) handlingQuotaMetrics(quotas []*node.Node, quotaCount *int, numMe
 				if value := strings.Split(attrValue, " ")[0]; value != "" {
 					// Few quota metrics would have value '-' which means unlimited (ex: disk-limit)
 					if value == "-" {
-						value = "-1"
+						// In 22.05, populate metrics value with 0
+						if q.historicalLabels {
+							value = "0"
+						} else {
+							value = "-1"
+						}
 					}
 
 					if attribute == "soft-disk-limit" || attribute == "disk-limit" || attribute == "disk-used" {
