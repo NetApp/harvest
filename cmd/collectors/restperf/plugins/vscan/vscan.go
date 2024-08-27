5f933ced2bc18c9d3eabd100d6692fe8fa68c64f
Chris Grindstaff <chris@gstaff.org>
2024-08-27 08:08:45 -0400

    feat: Harvest should include a Vscan dashboard

--- cmd/collectors/restperf/plugins/vscan/vscan.go
+++ cmd/collectors/restperf/plugins/vscan/vscan.go
@@ -6,7 +6,6 @@ import (
 	"github.com/netapp/harvest/v2/pkg/matrix"
 	"github.com/netapp/harvest/v2/pkg/util"
 	"strconv"
-	"strings"
 )
 
 type Vscan struct {
@@ -36,7 +35,7 @@ func (v *Vscan) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.
 		return nil, nil, nil
 	}
 
-	return v.aggregatePerScanner(data)
+	return collectors.AggregatePerScanner(v.Logger, data, "scan.latency", "scan.request_dispatched_rate")
 }
 
 func (v *Vscan) addSvmAndScannerLabels(data *matrix.Matrix) {
@@ -51,159 +50,4 @@ func (v *Vscan) addSvmAndScannerLabels(data *matrix.Matrix) {
 		instance.SetLabel("scanner", names.Scanner)
 		instance.SetLabel("node", names.Node)
 	}
-}
-
-func (v *Vscan) aggregatePerScanner(data *matrix.Matrix) ([]*matrix.Matrix, *util.Metadata, error) {
-	// When isPerScanner=true, Harvest 1.6 uses this form:
-	// netapp.perf.dev.nltl-fas2520.vscan.scanner.10_64_30_62.scanner_stats_pct_mem_used 18 1501765640
-
-	// These counters are per scanner and need averaging:
-	// 		scanner_stats_pct_cpu_used
-	// 		scanner_stats_pct_mem_used
-	// 		scanner_stats_pct_network_used
-	// These counters need to be summed:
-	// 		scan_request_dispatched_rate
-	// These counters need weighted averages:
-	// 		scan_latency
-
-	// create per scanner instance cache
-	cache := data.Clone(matrix.With{Data: false, Metrics: true, Instances: false, ExportInstances: true})
-	var err error
-	cache.UUID += ".Vscan"
-	opsKeyPrefix := "temp_"
-
-	for _, i := range data.GetInstances() {
-		scanner := i.GetLabel("scanner")
-		if cache.GetInstance(scanner) == nil {
-			s, _ := cache.NewInstance(scanner)
-			s.SetLabel("scanner", scanner)
-		}
-		i.SetExportable(false)
-	}
-
-	// aggregate per scanner
-	counts := make(map[string]map[string]int) // map[scanner][counter] => value
-
-	for _, i := range data.GetInstances() {
-		scanner := i.GetLabel("scanner")
-		ps := cache.GetInstance(scanner)
-		if ps == nil {
-			v.Logger.Error().Str("scanner", scanner).Msg("Failed to find scanner instance in cache")
-			continue
-		}
-		_, ok := counts[scanner]
-		if !ok {
-			counts[scanner] = make(map[string]int)
-		}
-		for mKey, m := range data.GetMetrics() {
-			if !m.IsExportable() && m.GetType() != "float64" {
-				continue
-			}
-			psm := cache.GetMetric(mKey)
-			if psm == nil {
-				v.Logger.Error().Str("scanner", scanner).Str("metric", mKey).
-					Msg("Failed to find metric in scanner cache")
-				continue
-			}
-			if value, ok := m.GetValueFloat64(i); ok {
-				fv, _ := psm.GetValueFloat64(ps)
-
-				if mKey == "scan.latency" {
-					// weighted average for scan.latency
-					opsKey := m.GetComment()
-
-					if ops := data.GetMetric(opsKey); ops != nil {
-						if opsValue, ok := ops.GetValueFloat64(i); ok {
-							var tempOpsV float64
-
-							prod := value * opsValue
-							tempOpsKey := opsKeyPrefix + opsKey
-							tempOps := cache.GetMetric(tempOpsKey)
-
-							if tempOps == nil {
-								if tempOps, err = cache.NewMetricFloat64(tempOpsKey); err != nil {
-									return nil, nil, err
-								}
-								tempOps.SetExportable(false)
-							} else {
-								tempOpsV, _ = tempOps.GetValueFloat64(ps)
-							}
-
-							if value != 0 {
-								err = tempOps.SetValueFloat64(ps, tempOpsV+opsValue)
-								if err != nil {
-									v.Logger.Error().Err(err).Msg("error")
-								}
-							}
-							err = psm.SetValueFloat64(ps, fv+prod)
-							if err != nil {
-								v.Logger.Error().Err(err).Msg("error")
-							}
-						}
-					}
-
-					continue
-				}
-				// sum for scan_request_dispatched_rate
-				if mKey == "scan.request_dispatched_rate" {
-					err := psm.SetValueFloat64(ps, fv+value)
-					if err != nil {
-						v.Logger.Error().Err(err).Str("metric", "scan_request_dispatched_rate").
-							Msg("Error setting metric value")
-					}
-
-					continue
-				} else if strings.HasSuffix(mKey, "_used") {
-					// these need averaging
-					counts[scanner][mKey]++
-					runningTotal, _ := psm.GetValueFloat64(ps)
-					value, _ := m.GetValueFloat64(ps)
-					err := psm.SetValueFloat64(ps, runningTotal+value)
-					if err != nil {
-						v.Logger.Error().Err(err).Str("mKey", mKey).Msg("Failed to set value")
-					}
-				}
-			}
-		}
-	}
-
-	// cook averaged values and latencies
-	for scanner, i := range cache.GetInstances() {
-		for mKey, m := range cache.GetMetrics() {
-			if !m.IsExportable() {
-				continue
-			}
-			if strings.HasSuffix(m.GetName(), "_used") {
-				count := counts[scanner][mKey]
-				value, ok := m.GetValueFloat64(i)
-				if !ok {
-					continue
-				}
-				if err := m.SetValueFloat64(i, value/float64(count)); err != nil {
-					v.Logger.Error().Err(err).
-						Str("mKey", mKey).
-						Str("name", m.GetName()).
-						Msg("Unable to set average")
-				}
-			} else if strings.HasSuffix(m.GetName(), "_latency") {
-				if value, ok := m.GetValueFloat64(i); ok {
-					opsKey := m.GetComment()
-
-					if ops := cache.GetMetric(opsKeyPrefix + opsKey); ops != nil {
-						if opsValue, ok := ops.GetValueFloat64(i); ok && opsValue != 0 {
-							err := m.SetValueFloat64(i, value/opsValue)
-							if err != nil {
-								v.Logger.Error().Err(err).Msg("error")
-							}
-						} else {
-							m.SetValueNAN(i)
-						}
-					}
-				}
-
-			}
-		}
-	}
-
-	return []*matrix.Matrix{cache}, nil, nil
 }
