6015a4ede8bb18a09e8fd0df689bec8cdb7f4a8f
Chris Grindstaff <chris@gstaff.org>
2025-05-16 11:31:31 -0400

    feat: Cisco collector should collect CDP and LLDP metrics

---
+++ cmd/collectors/cisco/plugins/cdp/cdp.go
@@ -0,0 +1,176 @@
+package cdp
+
+import (
+	"fmt"
+	"github.com/netapp/harvest/v2/cmd/collectors/cisco/rest"
+	"github.com/netapp/harvest/v2/cmd/poller/plugin"
+	"github.com/netapp/harvest/v2/pkg/conf"
+	"github.com/netapp/harvest/v2/pkg/matrix"
+	"github.com/netapp/harvest/v2/pkg/util"
+	"github.com/netapp/harvest/v2/third_party/tidwall/gjson"
+	"log/slog"
+	"slices"
+	"strings"
+	"time"
+)
+
+const (
+	labels = "labels"
+)
+
+type CDP struct {
+	*plugin.AbstractPlugin
+	matrix         *matrix.Matrix
+	client         *rest.Client
+	templateObject string // object name from the template
+}
+
+func New(p *plugin.AbstractPlugin) plugin.Plugin {
+	return &CDP{AbstractPlugin: p}
+}
+
+func (c *CDP) Init(_ conf.Remote) error {
+	var (
+		client *rest.Client
+		err    error
+	)
+
+	if err = c.InitAbc(); err != nil {
+		return fmt.Errorf("failed to initialize AbstractPlugin: %w", err)
+	}
+
+	timeout, _ := time.ParseDuration(rest.DefaultTimeout)
+
+	if client, err = rest.New(conf.ZapiPoller(c.ParentParams), timeout, c.Auth); err != nil {
+		return fmt.Errorf("error creating new client: %w", err)
+	}
+
+	c.client = client
+	c.templateObject = c.ParentParams.GetChildContentS("object")
+
+	c.matrix = matrix.New(c.Parent+".CDP", c.templateObject, c.templateObject)
+
+	return nil
+}
+
+func (c *CDP) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.Metadata, error) {
+	data := dataMap[c.Object]
+	c.client.Metadata.Reset()
+
+	versionMat, err := c.initMatrix(c.templateObject)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error while initializing matrix: %w", err)
+	}
+
+	// Set all global labels if they don't already exist
+	versionMat.SetGlobalLabels(data.GetGlobalLabels())
+
+	data.Reset()
+
+	command := c.ParentParams.GetChildContentS("query")
+	output, err := c.client.CLIShowArray(command)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to fetch data: %w", err)
+	}
+
+	c.parseCDP(output, versionMat)
+
+	c.client.Metadata.NumCalls = 1
+	c.client.Metadata.BytesRx = uint64(len(output.Raw))
+	c.client.Metadata.PluginInstances = uint64(len(versionMat.GetInstances()))
+
+	return []*matrix.Matrix{versionMat}, c.client.Metadata, nil
+}
+
+func (c *CDP) initMatrix(name string) (*matrix.Matrix, error) {
+
+	mat := matrix.New(c.Parent+name, name, name)
+
+	if err := matrix.CreateMetric(labels, mat); err != nil {
+		return nil, fmt.Errorf("error while creating metric %s: %w", labels, err)
+	}
+
+	return mat, nil
+}
+
+func (c *CDP) parseCDP(output gjson.Result, mat *matrix.Matrix) {
+
+	rowQuery := "output.body.TABLE_cdp_neighbor_detail_info.ROW_cdp_neighbor_detail_info"
+
+	var models []Model
+
+	rows := output.Get(rowQuery)
+
+	if !rows.Exists() {
+		c.SLogger.Warn("Unable to parse CDP because rows are missing", slog.String("query", rowQuery))
+		return
+	}
+
+	rows.ForEach(func(_, value gjson.Result) bool {
+		opticModel := NewCDPModel(value)
+		// Skip empty models
+		if opticModel.DeviceID == "" {
+			return true
+		}
+		models = append(models, opticModel)
+		return true
+	})
+
+	for _, model := range models {
+		instanceKey := model.DeviceID + model.PortID
+		instance, err := mat.NewInstance(instanceKey)
+		if err != nil {
+			c.SLogger.Warn("Failed to create cdp instance", slog.String("key", instanceKey))
+			return
+		}
+
+		instance.SetLabel("capabilities", strings.Join(model.Capabilities, ","))
+		instance.SetLabel("device_id", model.DeviceID)
+		instance.SetLabel("local_interface_mac", model.LocalInterfaceMAC)
+		instance.SetLabel("platform_id", model.PlatformID)
+		instance.SetLabel("port_id", model.PortID)
+		instance.SetLabel("remote_interface_mac", model.RemoteInterfaceMAC)
+		instance.SetLabel("version", model.Version)
+
+		mat.GetMetric(labels).SetValueFloat64(instance, 1.0)
+	}
+}
+
+type Model struct {
+	Capabilities       []string
+	DeviceID           string
+	LocalInterfaceMAC  string
+	PlatformID         string
+	PortID             string
+	RemoteInterfaceMAC string
+	TTL                int64
+	Version            string
+}
+
+func NewCDPModel(output gjson.Result) Model {
+
+	var m Model
+
+	m.DeviceID = output.Get("device_id").ClonedString()
+	m.PlatformID = output.Get("platform_id").ClonedString()
+	m.PortID = output.Get("port_id").ClonedString()
+	m.TTL = output.Get("ttl").Int()
+	m.Version = output.Get("version").ClonedString()
+	m.LocalInterfaceMAC = output.Get("local_intf_mac").ClonedString()
+	m.RemoteInterfaceMAC = output.Get("remote_intf_mac").ClonedString()
+
+	caps := output.Get("capability")
+	if caps.IsArray() {
+		caps.ForEach(func(_, value gjson.Result) bool {
+			m.Capabilities = append(m.Capabilities, value.String())
+			return true
+		})
+	} else if caps.Exists() {
+		m.Capabilities = []string{caps.ClonedString()}
+	}
+
+	slices.Sort(m.Capabilities)
+
+	return m
+}
---
+++ cmd/collectors/cisco/plugins/lldp/lldp.go
@@ -0,0 +1,211 @@
+package lldp
+
+import (
+	"fmt"
+	"github.com/netapp/harvest/v2/cmd/collectors/cisco/rest"
+	"github.com/netapp/harvest/v2/cmd/poller/plugin"
+	"github.com/netapp/harvest/v2/pkg/conf"
+	"github.com/netapp/harvest/v2/pkg/matrix"
+	"github.com/netapp/harvest/v2/pkg/util"
+	"github.com/netapp/harvest/v2/third_party/tidwall/gjson"
+	"log/slog"
+	"slices"
+	"strings"
+	"time"
+)
+
+const (
+	labels = "labels"
+)
+
+type LLDP struct {
+	*plugin.AbstractPlugin
+	matrix         *matrix.Matrix
+	client         *rest.Client
+	templateObject string // object name from the template
+}
+
+func New(p *plugin.AbstractPlugin) plugin.Plugin {
+	return &LLDP{AbstractPlugin: p}
+}
+
+func (l *LLDP) Init(_ conf.Remote) error {
+	var (
+		client *rest.Client
+		err    error
+	)
+
+	if err = l.InitAbc(); err != nil {
+		return fmt.Errorf("failed to initialize AbstractPlugin: %w", err)
+	}
+
+	timeout, _ := time.ParseDuration(rest.DefaultTimeout)
+
+	if client, err = rest.New(conf.ZapiPoller(l.ParentParams), timeout, l.Auth); err != nil {
+		return fmt.Errorf("error creating new client: %w", err)
+	}
+
+	l.client = client
+	l.templateObject = l.ParentParams.GetChildContentS("object")
+
+	l.matrix = matrix.New(l.Parent+".LLDP", l.templateObject, l.templateObject)
+
+	return nil
+}
+
+func (l *LLDP) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *util.Metadata, error) {
+	data := dataMap[l.Object]
+	l.client.Metadata.Reset()
+
+	versionMat, err := l.initMatrix(l.templateObject)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error while initializing matrix: %w", err)
+	}
+
+	// Set all global labels if they don't already exist
+	versionMat.SetGlobalLabels(data.GetGlobalLabels())
+
+	data.Reset()
+
+	command := l.ParentParams.GetChildContentS("query")
+	output, err := l.client.CLIShowArray(command)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to fetch data: %w", err)
+	}
+
+	l.parseLLDP(output, versionMat)
+
+	l.client.Metadata.NumCalls = 1
+	l.client.Metadata.BytesRx = uint64(len(output.Raw))
+	l.client.Metadata.PluginInstances = uint64(len(versionMat.GetInstances()))
+
+	return []*matrix.Matrix{versionMat}, l.client.Metadata, nil
+}
+
+func (l *LLDP) initMatrix(name string) (*matrix.Matrix, error) {
+
+	mat := matrix.New(l.Parent+name, name, name)
+
+	if err := matrix.CreateMetric(labels, mat); err != nil {
+		return nil, fmt.Errorf("error while creating metric %s: %w", labels, err)
+	}
+
+	return mat, nil
+}
+
+func (l *LLDP) parseLLDP(output gjson.Result, mat *matrix.Matrix) {
+
+	rowQuery := "output.body.TABLE_nbor_detail.ROW_nbor_detail"
+
+	var models []Model
+
+	rows := output.Get(rowQuery)
+
+	if !rows.Exists() {
+		l.SLogger.Warn("Unable to parse LLDP because rows are missing", slog.String("query", rowQuery))
+		return
+	}
+
+	rows.ForEach(func(_, value gjson.Result) bool {
+		opticModel := NewLLDPModel(value)
+		// Skip empty models
+		if opticModel.DeviceID == "" {
+			return true
+		}
+		models = append(models, opticModel)
+		return true
+	})
+
+	for _, model := range models {
+		instanceKey := model.ChassisID
+		instance, err := mat.NewInstance(instanceKey)
+		if err != nil {
+			l.SLogger.Warn("Failed to create lldp instance", slog.String("key", instanceKey))
+			return
+		}
+
+		instance.SetLabel("device_id", model.DeviceID)
+		instance.SetLabel("description", model.Description)
+		instance.SetLabel("chassis", model.ChassisID)
+		instance.SetLabel("local_interface", model.LocalInterface)
+		instance.SetLabel("port_id", model.PortID)
+		instance.SetLabel("capabilities", strings.Join(model.Capabilities, ","))
+
+		mat.GetMetric(labels).SetValueFloat64(instance, 1.0)
+	}
+}
+
+type Model struct {
+	Capabilities   []string
+	ChassisID      string
+	Description    string
+	DeviceID       string
+	LocalInterface string
+	PortID         string
+	TTL            int64
+}
+
+func NewLLDPModel(output gjson.Result) Model {
+
+	var m Model
+
+	m.DeviceID = output.Get("sys_name").ClonedString()
+	m.Description = output.Get("sys_desc").ClonedString()
+	m.ChassisID = output.Get("chassis_id").ClonedString()
+	m.LocalInterface = output.Get("l_port_id").ClonedString()
+	m.TTL = output.Get("ttl").Int()
+	m.PortID = output.Get("port_id").ClonedString()
+	m.Capabilities = lldpCapabilities(output.Get("enabled_capability").String())
+
+	return m
+}
+
+func lldpCapabilities(capStr string) []string {
+	// show lldp neighbors detail
+	// "system_capability" : "B, R",
+	// Capability codes:
+	//  (R) Router, (B) Bridge, (T) Telephone, (C) DOCSIS Cable Device
+	//  (W) WLAN Access Point, (P) Repeater, (S) Station, (O) Other
+
+	var (
+		capabilities []string
+		code         string
+	)
+
+	splits := strings.Split(capStr, ",")
+	for _, split := range splits {
+		letter := strings.TrimSpace(split)
+		// Ignore empty strings
+		if letter == "" {
+			continue
+		}
+
+		switch letter {
+		case "R":
+			code = "Router"
+		case "B":
+			code = "Bridge"
+		case "T":
+			code = "Telephone"
+		case "C":
+			code = "DOCSIS Cable Device"
+		case "W":
+			code = "WLAN Access Point"
+		case "P":
+			code = "Repeater"
+		case "S":
+			code = "Station"
+		case "O":
+			code = "Other"
+		}
+
+		if code != "" {
+			capabilities = append(capabilities, code)
+		}
+	}
+
+	slices.Sort(capabilities)
+
+	return capabilities
+}
