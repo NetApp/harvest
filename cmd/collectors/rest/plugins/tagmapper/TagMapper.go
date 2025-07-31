62ae769ba90a7dae994d55f0b90f9514288cdbe4
rahulguptajss <rahul.gupta@netapp.com>
2025-07-31 12:18:55 +0530

    feat: tags as labels for volume

--- cmd/collectors/rest/plugins/tagmapper/TagMapper.go
+++ cmd/collectors/rest/plugins/tagmapper/TagMapper.go
@@ -78,7 +78,7 @@ func (t *TagMapper) parseTagsToMap(tags string) map[string]string {
 	}
 	pairs := strings.Split(tags, ",")
 	for _, pair := range pairs {
-		parts := strings.SplitN(strings.TrimSpace(pair), ":", 2)
+		parts := strings.SplitN(pair, ":", 2)
 		if len(parts) == 2 {
 			key := strings.TrimSpace(parts[0])
 			value := strings.TrimSpace(parts[1])
