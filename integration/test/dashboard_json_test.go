1d94dc70a787de46cd1f285c4ae766ecb74a2c2c
rahulguptajss <rahul.gupta@netapp.com>
2025-09-10 15:05:35 +0530

    feat: collect Volume SnapLock information

--- integration/test/dashboard_json_test.go
+++ integration/test/dashboard_json_test.go
@@ -50,6 +50,7 @@ var zapiCounterMap = map[string]struct{}{
 	"volume_capacity_tier_footprint_percent": {},
 	"volume_num_compress_attempts":           {},
 	"volume_num_compress_fail":               {},
+	"volume_snaplock_labels":                 {},
 	// sar is experiencing high api time for ZapiPerf. The u2 cluster does not have fabricpool added for the collection of these counters. Remove the following once sar is capable of running ZapiPerf.
 	"volume_performance_tier_footprint":         {},
 	"volume_performance_tier_footprint_percent": {},
