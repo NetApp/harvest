f1d5534dfab89267b86970344f3956e0f3b8f6f4
Chris Grindstaff <chris@gstaff.org>
2026-09-01 13:10:23 -0400

    fix: collector should sum pluginInstances across all plugins

--- cmd/collectors/restperf/plugins/disk/disk.go
+++ cmd/collectors/restperf/plugins/disk/disk.go
@@ -424,6 +424,12 @@ func (d *Disk) Run(dataMap map[string]*matrix.Matrix) ([]*matrix.Matrix, *collec
 		return output, nil, err
 	}
 
+	var pluginInstances uint64
+	for _, mat := range output {
+		pluginInstances += uint64(len(mat.GetInstances()))
+	}
+	d.RequestMetadata.PluginInstances.Store(pluginInstances)
+
 	return output, &d.RequestMetadata, nil
 }
 
